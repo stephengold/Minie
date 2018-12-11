cc5692d033664b49cc868f22ea5bffe2ede2198a
Stephen Gold <sgold@sonic.net>
2018-12-11 10:30:46 -0800

    added TestCloneCharacter and TestCloneGhost

---
+++ MinieLibrary/src/test/java/jme3utilities/minie/test/TestCloneCharacter.java
@@ -0,0 +1,155 @@
+/*
+ Copyright (c) 2018, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.DesktopAssetManager;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.objects.PhysicsCharacter;
+import com.jme3.export.binary.BinaryLoader;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.system.NativeLibraryLoader;
+import jme3utilities.Misc;
+import org.junit.Test;
+
+/**
+ * Test cloning a PhysicsCharacter.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestCloneCharacter {
+
+    private AssetManager assetManager;
+    // *************************************************************************
+    // new methods exposed
+
+    @Test
+    public void testCloneCharacter() {
+        NativeLibraryLoader.loadNativeLibrary("bulletjme", true);
+        assetManager = new DesktopAssetManager();
+        assetManager.registerLoader(BinaryLoader.class, "j3o");
+        assetManager.registerLocator(".", FileLocator.class);
+
+        CollisionShape shape = new SphereCollisionShape(1f);
+        PhysicsCharacter ch = new PhysicsCharacter(shape, 1f);
+        setParameters(ch, 0f);
+        verifyParameters(ch, 0f);
+
+        PhysicsCharacter chClone = (PhysicsCharacter) Misc.deepCopy(ch);
+        cloneTest(ch, chClone);
+    }
+    // *************************************************************************
+    // private methods
+
+    private void cloneTest(PhysicsCharacter ch, PhysicsCharacter chClone) {
+        assert chClone.getObjectId() != ch.getObjectId();
+        assert chClone.getControllerId() != ch.getControllerId();
+
+        verifyParameters(ch, 0f);
+        verifyParameters(chClone, 0f);
+
+        setParameters(ch, 0.3f);
+        verifyParameters(ch, 0.3f);
+        verifyParameters(chClone, 0f);
+
+        setParameters(chClone, 0.6f);
+        verifyParameters(ch, 0.3f);
+        verifyParameters(chClone, 0.6f);
+    }
+
+    private void setParameters(PhysicsCharacter ch, float b) {
+        boolean flag = (b > 0.15f && b < 0.45f);
+        ch.setContactResponse(flag);
+
+        ch.setAngularDamping(b + 0.01f);
+        ch.setAngularVelocity(new Vector3f(b + 0.04f, b + 0.05f, b + 0.06f));
+        ch.setCcdMotionThreshold(b + 0.07f);
+        ch.setCcdSweptSphereRadius(b + 0.08f);
+        ch.setFallSpeed(b + 0.09f);
+        ch.setGravity(new Vector3f(b + 0.10f, b + 0.11f, b + 0.12f));
+        ch.setJumpSpeed(b + 0.125f);
+        ch.setLinearDamping(b + 0.13f);
+        /*
+         * Walk direction affects linear velocity, so set it first!
+         */
+        ch.setWalkDirection(new Vector3f(b + 0.291f, b + 0.292f, b + 0.293f));
+        ch.setLinearVelocity(new Vector3f(b + 0.26f, b + 0.27f, b + 0.28f));
+
+        ch.setMaxPenetrationDepth(b + 0.281f);
+        ch.setMaxSlope(b + 0.282f);
+        ch.setPhysicsLocation(new Vector3f(b + 0.18f, b + 0.19f, b + 0.20f));
+        ch.setStepHeight(b + 0.29f);
+    }
+
+    private void verifyParameters(PhysicsCharacter ch, float b) {
+        boolean flag = (b > 0.15f && b < 0.45f);
+        assert ch.isContactResponse() == flag;
+
+        assert ch.getAngularDamping() == b + 0.01f;
+
+        Vector3f w = ch.getAngularVelocity(null);
+        assert w.x == b + 0.04f : w;
+        assert w.y == b + 0.05f : w;
+        assert w.z == b + 0.06f : w;
+
+        assert ch.getCcdMotionThreshold() == b + 0.07f;
+        assert ch.getCcdSweptSphereRadius() == b + 0.08f;
+
+        assert ch.getFallSpeed() == b + 0.09f;
+
+        Vector3f g = ch.getGravity(null);
+        assert FastMath.approximateEquals(g.x, b + 0.10f) : g;
+        assert FastMath.approximateEquals(g.y, b + 0.11f) : g;
+        assert FastMath.approximateEquals(g.z, b + 0.12f) : g;
+
+        assert ch.getJumpSpeed() == b + 0.125f;
+        assert ch.getLinearDamping() == b + 0.13f;
+
+        Vector3f v = ch.getLinearVelocity(null);
+        assert FastMath.approximateEquals(v.x, b + 0.26f) : v;
+        assert FastMath.approximateEquals(v.y, b + 0.27f) : v;
+        assert FastMath.approximateEquals(v.z, b + 0.28f) : v;
+
+        assert ch.getMaxPenetrationDepth() == b + 0.281f;
+        assert ch.getMaxSlope() == b + 0.282f;
+
+        Vector3f x = ch.getPhysicsLocation(null);
+        assert x.x == b + 0.18f : x;
+        assert x.y == b + 0.19f : x;
+        assert x.z == b + 0.20f : x;
+
+        assert ch.getStepHeight() == b + 0.29f;
+
+        Vector3f d = ch.getWalkDirection();
+        assert d.x == b + 0.291f : d;
+        assert d.y == b + 0.292f : d;
+        assert d.z == b + 0.293f : d;
+    }
+}
---
+++ MinieLibrary/src/test/java/jme3utilities/minie/test/TestCloneGhost.java
@@ -0,0 +1,185 @@
+/*
+ Copyright (c) 2018, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.AssetNotFoundException;
+import com.jme3.asset.DesktopAssetManager;
+import com.jme3.asset.ModelKey;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.GhostControl;
+import com.jme3.bullet.objects.PhysicsGhostObject;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.binary.BinaryExporter;
+import com.jme3.export.binary.BinaryLoader;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.Control;
+import com.jme3.system.NativeLibraryLoader;
+import java.io.File;
+import java.io.IOException;
+import jme3utilities.Misc;
+import org.junit.Test;
+
+/**
+ * Test cloning/saving/loading on PhysicsGhostObject and its subclass.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestCloneGhost {
+
+    private AssetManager assetManager;
+    private int fileIndex = 0;
+    // *************************************************************************
+    // new methods exposed
+
+    @Test
+    public void testCloneGhost() {
+        NativeLibraryLoader.loadNativeLibrary("bulletjme", true);
+        assetManager = new DesktopAssetManager();
+        assetManager.registerLoader(BinaryLoader.class, "j3o");
+        assetManager.registerLocator(".", FileLocator.class);
+
+        CollisionShape shape = new SphereCollisionShape(1f);
+        /*
+         * PhysicsGhostObject
+         */
+        PhysicsGhostObject pgo = new PhysicsGhostObject(shape);
+        setParameters(pgo, 0f);
+        verifyParameters(pgo, 0f);
+        PhysicsGhostObject pgoClone = (PhysicsGhostObject) Misc.deepCopy(pgo);
+        cloneTest(pgo, pgoClone);
+        /*
+         * GhostControl
+         */
+        GhostControl gc = new GhostControl(shape);
+        setParameters(gc, 0f);
+        verifyParameters(gc, 0f);
+        GhostControl gcClone = (GhostControl) Misc.deepCopy(gc);
+        cloneTest(gc, gcClone);
+    }
+    // *************************************************************************
+    // private methods
+
+    private void cloneTest(PhysicsGhostObject pgo,
+            PhysicsGhostObject pgoClone) {
+        assert pgoClone.getObjectId() != pgo.getObjectId();
+
+        verifyParameters(pgo, 0f);
+        verifyParameters(pgoClone, 0f);
+
+        setParameters(pgo, 0.3f);
+        verifyParameters(pgo, 0.3f);
+        verifyParameters(pgoClone, 0f);
+
+        setParameters(pgoClone, 0.6f);
+        verifyParameters(pgo, 0.3f);
+        verifyParameters(pgoClone, 0.6f);
+
+        if (pgo instanceof GhostControl) {
+            GhostControl pgoCopy = saveThenLoad((GhostControl) pgo);
+            verifyParameters(pgoCopy, 0.3f);
+
+            GhostControl gcCloneCopy = saveThenLoad((GhostControl) pgoClone);
+            verifyParameters(gcCloneCopy, 0.6f);
+        }
+    }
+
+    /**
+     * Clone a GhostControl by saving and then loading it.
+     *
+     * @param gc the control to copy (not null, unaffected)
+     * @return a new control
+     */
+    private GhostControl saveThenLoad(GhostControl gc) {
+        Node savedNode = new Node();
+        /*
+         * Add the Control to the Node without altering its physics transform.
+         */
+        Vector3f pl = gc.getPhysicsLocation(null);
+        Matrix3f pr = gc.getPhysicsRotationMatrix(null);
+        savedNode.addControl(gc);
+        gc.setPhysicsLocation(pl);
+        gc.setPhysicsRotation(pr);
+
+        String fileName = String.format("tmp%d.j3o", ++fileIndex);
+        File file = new File(fileName);
+
+        JmeExporter exporter = BinaryExporter.getInstance();
+        try {
+            exporter.save(savedNode, file);
+        } catch (IOException exception) {
+            assert false;
+        }
+
+        ModelKey key = new ModelKey(fileName);
+        Spatial loadedNode = new Node();
+        try {
+            loadedNode = assetManager.loadAsset(key);
+        } catch (AssetNotFoundException e) {
+            assert false;
+        }
+        file.delete();
+        Control loadedSgc = loadedNode.getControl(0);
+
+        return (GhostControl) loadedSgc;
+    }
+
+    private void setParameters(PhysicsGhostObject pgo, float b) {
+        pgo.setCcdMotionThreshold(b + 0.07f);
+        pgo.setCcdSweptSphereRadius(b + 0.08f);
+        pgo.setPhysicsLocation(new Vector3f(b + 0.18f, b + 0.19f, b + 0.20f));
+
+        Quaternion orient
+                = new Quaternion(b + 0.21f, b + 0.22f, b + 0.23f, b + 0.24f);
+        orient.normalizeLocal();
+        Matrix3f matrix = orient.toRotationMatrix();
+        pgo.setPhysicsRotation(matrix);
+    }
+
+    private void verifyParameters(PhysicsGhostObject pgo, float b) {
+        assert pgo.getCcdMotionThreshold() == b + 0.07f;
+        assert pgo.getCcdSweptSphereRadius() == b + 0.08f;
+
+        Vector3f x = pgo.getPhysicsLocation(null);
+        assert x.x == b + 0.18f : x;
+        assert x.y == b + 0.19f : x;
+        assert x.z == b + 0.20f : x;
+
+        Quaternion orient
+                = new Quaternion(b + 0.21f, b + 0.22f, b + 0.23f, b + 0.24f);
+        orient.normalizeLocal();
+        Matrix3f matrix = orient.toRotationMatrix();
+        Matrix3f m = pgo.getPhysicsRotationMatrix(null);
+        assert m.equals(matrix);
+    }
+}
