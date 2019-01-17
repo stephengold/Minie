7bb59db885e0cf14e6823339f3f5098cc1830269
Stephen Gold <sgold@sonic.net>
2019-01-17 10:04:44 -0800

    MultiSphere: simplify/clarify

--- MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/MultiSphere.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/MultiSphere.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 jMonkeyEngine
+ * Copyright (c) 2018-2019 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -252,7 +252,7 @@ public class MultiSphere extends CollisionShape {
          */
         centers = new Vector3f[4];
         radii = new float[4];
-        for (int sphereI = 0; sphereI < centerLocations.size(); sphereI++) {
+        for (int sphereI = 0; sphereI < 4; sphereI++) {
             Vector3f localCenter = centerLocations.get(sphereI);
             centers[sphereI] = rectangularSolid.localToWorld(localCenter, null);
             radii[sphereI] = radius;
