061bf8d6ec4504a2b4d56c4e2ad6a2fa742fe8e6
Stephen Gold <sgold@sonic.net>
2019-01-27 20:54:04 -0800

    PhysicsSpace: remove a println() left over from debugging

--- MinieLibrary/src/main/java/com/jme3/bullet/PhysicsSpace.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/PhysicsSpace.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2018 jMonkeyEngine
+ * Copyright (c) 2009-2019 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -456,7 +456,6 @@ public class PhysicsSpace {
      */
     public static <V> Future<V> enqueueOnThisThread(Callable<V> callable) {
         AppTask<V> task = new AppTask<>(callable);
-        System.out.println("created apptask");
         pQueueTL.get().add(task);
 
         return task;
