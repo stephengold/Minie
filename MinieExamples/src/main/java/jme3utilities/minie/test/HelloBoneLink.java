4becf1889837342dd746813e6e17b72109080847
Stephen Gold <sgold@sonic.net>
2019-02-08 15:18:36 -0800

    HelloBoneLink: finalize variable to avoid a compile-time error from JDK7

--- MinieExamples/src/main/java/jme3utilities/minie/test/HelloBoneLink.java
+++ MinieExamples/src/main/java/jme3utilities/minie/test/HelloBoneLink.java
@@ -116,7 +116,7 @@ public class HelloBoneLink extends SimpleApplication {
         // Configure a DynamicAnimControl.
         LinkConfig defaultConfig = new LinkConfig();
         RangeOfMotion defaultRom = new RangeOfMotion(1f, 1f, 1f);
-        DynamicAnimControl dac = new DynamicAnimControl();
+        final DynamicAnimControl dac = new DynamicAnimControl();
         dac.link("Joint9", defaultConfig, defaultRom); // right shoulder
         dac.link("Joint11", defaultConfig, defaultRom); // right elbow
         dac.link("Joint12", defaultConfig, defaultRom); // right wrist
