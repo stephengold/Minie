3759709d5423a294720aacf024f9d12d3c2921fb
Stephen Gold <sgold@sonic.net>
2019-03-02 03:45:11 -0800

    add a TestSphereSphereCollision app

---
+++ MinieExamples/src/main/java/jme3utilities/minie/test/TestSphereSphereCollision.java
@@ -0,0 +1,136 @@
+/*
+ Copyright (c) 2019, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.PhysicsCollisionEvent;
+import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.objects.PhysicsGhostObject;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import java.util.logging.Logger;
+
+/**
+ * Test sphere-sphere collision reporting via a listener.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestSphereSphereCollision
+        extends SimpleApplication
+        implements PhysicsCollisionListener {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(TestSphereSphereCollision.class.getName());
+    // *************************************************************************
+    // fields
+
+    /**
+     * track elapsed time (in seconds) for the timeout
+     */
+    private double elapsedSeconds = 0.0;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the application.
+     *
+     * @param arguments array of command-line arguments (not null)
+     */
+    public static void main(String[] arguments) {
+        Application application = new TestSphereSphereCollision();
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.setDebugEnabled(true);
+
+        PhysicsSpace physicsSpace = bulletAppState.getPhysicsSpace();
+        physicsSpace.addCollisionListener(this);
+
+        CollisionShape shape;
+        shape = new SphereCollisionShape(1f);
+        //shape = new BoxCollisionShape(new Vector3f(1f, 1f, 1f));
+
+        PhysicsRigidBody staticBody = new PhysicsRigidBody(shape, 0f);
+        physicsSpace.add(staticBody);
+
+        PhysicsGhostObject ghost = new PhysicsGhostObject(shape);
+        physicsSpace.add(ghost);
+    }
+
+    /**
+     * Callback invoked once per frame.
+     *
+     * @param tpf time interval between frame (in seconds, &ge;0)
+     */
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+
+        elapsedSeconds += tpf;
+        if (elapsedSeconds > 1.0) {
+            throw new RuntimeException("No collisions reported!");
+        }
+    }
+    // *************************************************************************
+    // PhysicsCollisionListener methods
+
+    /**
+     * The collision listener, invoked multiple times for each collision that
+     * occurs in the PhysicsSpace.
+     *
+     * @param event the event that occurred (not null, reusable)
+     */
+    @Override
+    public void collision(PhysicsCollisionEvent event) {
+        Class aClass = event.getObjectA().getCollisionShape().getClass();
+        String aShape = aClass.getSimpleName().replace("CollisionShape", "");
+        Class bClass = event.getObjectB().getCollisionShape().getClass();
+        String bShape = bClass.getSimpleName().replace("CollisionShape", "");
+
+        System.out.printf("%s-%s collision reported at t = %f sec%n",
+                aShape, bShape, elapsedSeconds);
+        stop();
+    }
+}
