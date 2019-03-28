939406568d62397998a69da7712ac720ba7af015
Stephen Gold <sgold@sonic.net>
2019-03-28 16:16:36 -0700

    TrackDemo: progress update - still not satisfactory

--- MinieExamples/src/main/java/jme3utilities/minie/test/TrackDemo.java
+++ MinieExamples/src/main/java/jme3utilities/minie/test/TrackDemo.java
@@ -77,6 +77,7 @@ import jme3utilities.debug.SkeletonVisualizer;
 import jme3utilities.math.MyVector3f;
 import jme3utilities.minie.DumpFlags;
 import jme3utilities.minie.PhysicsDumper;
+import jme3utilities.minie.test.controllers.TrackController;
 import jme3utilities.minie.test.tunings.Binocular;
 import jme3utilities.minie.test.tunings.JaimeControl;
 import jme3utilities.minie.test.tunings.MhGameControl;
@@ -111,22 +112,23 @@ public class TrackDemo extends ActionApplication {
     // *************************************************************************
     // fields
 
+    /**
+     * true once {@link #initWhenReady()} has been invoked for the latest model
+     */
     private boolean dacReadyInitDone = false;
     /**
      * AppState to manage the PhysicsSpace
      */
     final private BulletAppState bulletAppState = new BulletAppState();
+    /**
+     * control being tested
+     */
     private DynamicAnimControl dac;
-    /*
-     * model height in world units
-     */
-    private float height;
-    /*
-     * world X coordinate for the model's center
-     */
-    private float xOffset;
-    private int chainLength;
 
+    private int chainLength;
+    /**
+     * C-G model on which the control is being tested
+     */
     private Node cgModel;
     /**
      * inverse kinematics joint for the finger/sword tip
@@ -163,16 +165,11 @@ public class TrackDemo extends ActionApplication {
     private Vector3f gridTopRight;
     private Vector3f gridBottomLeft;
     private Vector3f gridBottomRight;
-
-    public static Vector3f watchActual = new Vector3f(); //
-    private PointVisualizer actualPoint;
-    public static Vector3f watchDesired = new Vector3f();
-    private PointVisualizer desiredPoint;
     // *************************************************************************
     // new methods exposed
 
     /**
-     * Main entry point for the application.
+     * Main entry point for the TrackDemo application.
      *
      * @param ignored array of command-line arguments (not null)
      */
@@ -192,6 +189,7 @@ public class TrackDemo extends ActionApplication {
         settings.setTitle(applicationName);
 
         settings.setGammaCorrection(true);
+        settings.setSamples(4); // anti-aliasing
         settings.setVSync(true);
         application.setSettings(settings);
 
@@ -213,22 +211,13 @@ public class TrackDemo extends ActionApplication {
         ColorRGBA bgColor = new ColorRGBA(0.2f, 0.2f, 1f, 1f);
         viewPort.setBackgroundColor(bgColor);
         addLighting();
-
         stateManager.getState(StatsAppState.class).toggleStats();
         addAxes();
         addBox();
-        addGrid();
 
         //addModel("Sinbad");
         //addModel("MhGame");
         addModel("SinbadWith1Sword");
-
-        actualPoint = new PointVisualizer(assetManager, 16, ColorRGBA.Green,
-                "saltire"); //
-        rootNode.attachChild(actualPoint);
-        desiredPoint = new PointVisualizer(assetManager, 16, ColorRGBA.Green,
-                "ring"); //
-        rootNode.attachChild(desiredPoint);
         /*
          * Add a target rigid body, to be moved by dragging RMB.
          */
@@ -282,18 +271,22 @@ public class TrackDemo extends ActionApplication {
                 case "dump physicsSpace":
                     dumpPhysicsSpace();
                     return;
+
                 case "dump scenes":
                     dumpScenes();
                     return;
                 case "toggle meshes":
                     toggleMeshes();
                     return;
+
                 case "toggle pause":
                     togglePause();
                     return;
+
                 case "toggle physics debug":
                     togglePhysicsDebug();
                     return;
+
                 case "toggle skeleton":
                     toggleSkeleton();
                     return;
@@ -305,14 +298,13 @@ public class TrackDemo extends ActionApplication {
                 return;
             }
         }
-
         super.onAction(actionString, ongoing, tpf);
     }
 
     /**
      * Callback invoked once per frame.
      *
-     * @param tpf time interval between frames (in seconds, &ge;0)
+     * @param tpf the time interval between frames (in seconds, &ge;0)
      */
     @Override
     public void simpleUpdate(float tpf) {
@@ -327,10 +319,6 @@ public class TrackDemo extends ActionApplication {
             initWhenReady();
             dacReadyInitDone = true;
         }
-
-        MySpatial.setWorldLocation(actualPoint, watchActual); //
-        MySpatial.setWorldLocation(desiredPoint, watchDesired);
-
     }
     // *************************************************************************
     // private methods
@@ -348,7 +336,7 @@ public class TrackDemo extends ActionApplication {
     }
 
     /**
-     * Add a large static box to serve as a platform.
+     * Add a large static box to the scene, to serve as a platform.
      */
     private void addBox() {
         float halfExtent = 50f; // mesh units
@@ -373,34 +361,39 @@ public class TrackDemo extends ActionApplication {
     }
 
     /**
-     * Add an X-Z grid to the scene.
+     * Add a 10x10 X-Z square grid to the scene with the specified bottom-left
+     * and bottom-right corners, replacing any pre-existing grid.
      */
     private void addGrid() {
-        int gridColumns = 10;
-        int gridRows = 10;
-        float gridSpacing = 0.2f; // mesh units
-        Mesh mesh = new Grid(gridColumns + 1, gridRows + 1, gridSpacing);
+        assert gridBottomLeft.x == gridBottomRight.x;
+        assert gridBottomLeft.y == gridBottomRight.y;
+        assert gridBottomLeft.z < gridBottomRight.z;
+
+        Spatial old = MySpatial.findChild(rootNode, "grid");
+        if (old != null) {
+            old.removeFromParent();
+        }
+
+        int numColumns = 10;
+        int numRows = 10;
+        float gridSpacing = (gridBottomRight.z - gridBottomLeft.z) / numColumns;
+        Mesh mesh = new Grid(numColumns + 1, numRows + 1, gridSpacing);
         Geometry geometry = new Geometry("grid", mesh);
         rootNode.attachChild(geometry);
         geometry.rotate(0f, 0f, FastMath.HALF_PI);
 
-        ColorRGBA color = new ColorRGBA(0f, 0f, 0f, 1f);
-        Material mat = MyAsset.createWireframeMaterial(assetManager, color);
+        ColorRGBA gridColor = new ColorRGBA(0f, 0f, 0f, 1f);
+        Material mat = MyAsset.createWireframeMaterial(assetManager, gridColor);
         geometry.setMaterial(mat);
         /*
-         * Translate the origin of the grid.
+         * Translate the origin of the grid, which
+         * becomes its bottom-left corner.
          */
-        float bottomY = 0.5f;
-        float gridX = -1f;
-        float leftZ = -1f;
-        geometry.move(gridX, bottomY, leftZ);
-
-        float rightZ = leftZ + gridSpacing * gridColumns;
-        float topY = bottomY + gridSpacing * gridRows;
-        gridBottomLeft = new Vector3f(gridX, bottomY, leftZ);
-        gridBottomRight = new Vector3f(gridX, bottomY, rightZ);
-        gridTopLeft = new Vector3f(gridX, topY, leftZ);
-        gridTopRight = new Vector3f(gridX, topY, rightZ);
+        geometry.move(gridBottomLeft);
+        float gridX = gridBottomLeft.x;
+        float topY = gridBottomLeft.y + gridSpacing * numRows;
+        gridTopLeft = new Vector3f(gridX, topY, gridBottomLeft.z);
+        gridTopRight = new Vector3f(gridX, topY, gridBottomRight.z);
     }
 
     /**
@@ -432,10 +425,9 @@ public class TrackDemo extends ActionApplication {
             dac.getSpatial().removeControl(dac);
             rootNode.detachChild(cgModel);
             rootNode.removeControl(sv);
+            dacReadyInitDone = false;
         }
-        dacReadyInitDone = false;
-        xOffset = 0f;
-        chainLength = 6;
+        chainLength = 6; // constant?
 
         switch (modelName) {
             case "Jaime":
@@ -474,9 +466,9 @@ public class TrackDemo extends ActionApplication {
         cgModel.setCullHint(Spatial.CullHint.Never);
 
         rootNode.attachChild(cgModel);
+        float height = 2f;
         setHeight(cgModel, height);
         center(cgModel);
-        cgModel.move(xOffset, 0f, 0f);
 
         List<SkeletonControl> scList
                 = MySpatial.listControls(cgModel, SkeletonControl.class, null);
@@ -490,6 +482,8 @@ public class TrackDemo extends ActionApplication {
         sv = new SkeletonVisualizer(assetManager, sc);
         sv.setLineColor(ColorRGBA.Yellow); // TODO clean up visualization
         rootNode.addControl(sv);
+
+        addGrid();
     }
 
     /**
@@ -513,8 +507,8 @@ public class TrackDemo extends ActionApplication {
         flyCam.setDragToRotate(true);
         flyCam.setMoveSpeed(4f);
 
-        cam.setLocation(new Vector3f(-3.3f, 1.5f, -0.1f));
-        cam.setRotation(new Quaternion(0.08f, 0.687f, -0.06f, 0.72f));
+        cam.setLocation(new Vector3f(-4.3f, 2.1f, -0.2f));
+        cam.setRotation(new Quaternion(0.0643f, 0.69194f, -0.0456f, 0.71765f));
 
         CameraOrbitAppState orbitState
                 = new CameraOrbitAppState(cam, "orbitLeft", "orbitRight");
@@ -525,7 +519,7 @@ public class TrackDemo extends ActionApplication {
      * Configure physics during startup.
      */
     private void configurePhysics() {
-        CollisionShape.setDefaultMargin(0.005f); // 5 mm margin
+        CollisionShape.setDefaultMargin(0.005f); // 5-mm margin
         stateManager.attach(bulletAppState);
 
         physicsSpace = bulletAppState.getPhysicsSpace();
@@ -577,7 +571,7 @@ public class TrackDemo extends ActionApplication {
         watch.setEnabled(false);
         watch.setErrorGainFactor(0.1f);
         /*
-             * If the model's eyes are animated, each eye tracks the target.
+         * If the model's eyes are animated, each eye tracks the target.
          */
         if (dac instanceof Binocular) {
             Binocular binocular = (Binocular) dac;
@@ -622,10 +616,8 @@ public class TrackDemo extends ActionApplication {
 
         dac = new JaimeControl();
         faceDirection = Vector3f.UNIT_Z;
-        height = 2f;
         noseSpec = "122/JaimeGeom-geom-1"; // bridge of nose
         tipSpec = "2704/JaimeGeom-geom-1"; // tip of right index finger
-        //xOffset = 1f;
     }
 
     /**
@@ -635,9 +627,9 @@ public class TrackDemo extends ActionApplication {
         cgModel = (Node) assetManager.loadModel(
                 "Models/MhGame/MhGame.mesh.xml");
         cgModel.rotate(0f, -1.6f, 0f);
+
         dac = new MhGameControl();
         faceDirection = Vector3f.UNIT_Z;
-        height = 3f;
         noseSpec = "9313/male_generic"; // bridge of nose
         tipSpec = "5239/male_generic"; // tip of right index finger
     }
@@ -648,9 +640,9 @@ public class TrackDemo extends ActionApplication {
     private void loadNinja() {
         cgModel = (Node) assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
         cgModel.rotate(0f, 1.6f, 0f);
+
         dac = new NinjaControl();
         faceDirection = new Vector3f(0f, 0f, -1f);
-        height = 2f;
         noseSpec = "277/Ninja-geom-1"; // bridge of nose
         tipSpec = "55/Ninja-geom-2"; // tip of katana blade
     }
@@ -661,8 +653,9 @@ public class TrackDemo extends ActionApplication {
     private void loadOto() {
         cgModel = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
         cgModel.rotate(0f, -1.6f, 0f);
+
         dac = new OtoControl();
-        height = 3f;
+        faceDirection = new Vector3f(-1f, 0f, 0f);
         noseSpec = "161/Oto-geom-1"; // bridge of nose
         tipSpec = "3236/Oto-geom-1"; // right knuckle
     }
@@ -673,9 +666,9 @@ public class TrackDemo extends ActionApplication {
     private void loadPuppet() {
         cgModel = (Node) assetManager.loadModel("Models/Puppet/Puppet.j3o");
         cgModel.rotate(0f, -1.6f, 0f);
+
         dac = new PuppetControl();
         faceDirection = Vector3f.UNIT_Y;
-        height = 3f;
         noseSpec = "140/Mesh.011_0"; // bridge of nose
         tipSpec = "3185/Mesh.009_0"; // tip of right index finger
     }
@@ -687,11 +680,14 @@ public class TrackDemo extends ActionApplication {
         cgModel = (Node) assetManager.loadModel(
                 "Models/Sinbad/Sinbad.mesh.xml");
         cgModel.rotate(0f, -1.6f, 0f);
+
         dac = new SinbadControl();
         faceDirection = new Vector3f(0f, 1f, -3f).normalizeLocal();
-        height = 3f;
         noseSpec = "1844/Sinbad-geom-2"; // bridge of nose
         tipSpec = "223/Sinbad-geom-2"; // tip of right index finger
+
+        gridBottomLeft.set(-1f, 0.5f, -1f);
+        gridBottomRight.set(-1f, 0.5f, 1f);
     }
 
     /**
@@ -717,9 +713,10 @@ public class TrackDemo extends ActionApplication {
         dac.attach("Handle.R", swordConfig, sword);
 
         faceDirection = new Vector3f(0f, 1f, -3f).normalizeLocal();
-        height = 2f;
         noseSpec = "1844/Sinbad-geom-2"; // bridge of nose
         tipSpec = "4/Sword-geom-3/Handle.R"; // tip of sword blade
+        gridBottomLeft = new Vector3f(-1f, 0.5f, -1f);
+        gridBottomRight = new Vector3f(-1f, 0.5f, 1f);
     }
 
     /**
@@ -748,7 +745,6 @@ public class TrackDemo extends ActionApplication {
         faceDirection = new Vector3f(0f, 1f, -3f).normalizeLocal();
         noseSpec = "1844/Sinbad-geom-2"; // bridge of nose
         tipSpec = "4/Sword-geom-3/Handle.R"; // tip of right-hand sword blade
-        height = 2f;
     }
 
     /**
