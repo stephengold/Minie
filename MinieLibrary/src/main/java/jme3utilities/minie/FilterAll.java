43d6125e401b7cb7d5376062c5827bcf17a2353f
Stephen Gold <sgold@sonic.net>
2019-04-22 12:28:26 -0700

    move FilterAll class into the library

--- MinieLibrary/src/main/java/jme3utilities/minie/FilterAll.java
+++ MinieLibrary/src/main/java/jme3utilities/minie/FilterAll.java
@@ -24,7 +24,7 @@
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package jme3utilities.minie.test;
+package jme3utilities.minie;
 
 import com.jme3.bullet.debug.BulletDebugAppState;
 import com.jme3.export.Savable;
