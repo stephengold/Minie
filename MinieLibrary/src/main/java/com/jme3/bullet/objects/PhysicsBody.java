98629cbca8d739991c304ef3250f914ee526f44d
Stephen Gold <sgold@sonic.net>
2019-06-19 13:41:55 -0700

    PhysicsBody: publicize all methods

--- MinieLibrary/src/main/java/com/jme3/bullet/objects/PhysicsBody.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/objects/PhysicsBody.java
@@ -46,7 +46,7 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      *
      * @param joint the joint to add (not null)
      */
-    abstract void addJoint(PhysicsJoint joint);
+    abstract public void addJoint(PhysicsJoint joint);
 
     /**
      * Count how many joints connect to this body.
@@ -54,7 +54,7 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      * @return the count (&ge;0) or 0 if the body isn't added to any
      * PhysicsSpace
      */
-    abstract int countJoints();
+    abstract public int countJoints();
 
     /**
      * Copy this body's gravitational acceleration.
@@ -63,14 +63,14 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      * @return an acceleration vector (in physics-space coordinates, either
      * storeResult or a new vector, not null)
      */
-    abstract Vector3f getGravity(Vector3f storeResult);
+    abstract public Vector3f getGravity(Vector3f storeResult);
 
     /**
      * Determine the total mass of this body.
      *
      * @return the total mass (&ge;0)
      */
-    abstract float getMass();
+    abstract public float getMass();
 
     /**
      * Enumerate the joints connected to this body.
@@ -78,14 +78,14 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      * @return a new array of pre-existing joints, or null if this body is not
      * added to any space
      */
-    abstract PhysicsJoint[] listJoints();
+    abstract public PhysicsJoint[] listJoints();
 
     /**
      * Do not invoke directly! Joints are removed automatically when destroyed.
      *
      * @param joint the joint to remove (not null)
      */
-    abstract void removeJoint(PhysicsJoint joint);
+    abstract public void removeJoint(PhysicsJoint joint);
 
     /**
      * Alter this body's gravitational acceleration.
@@ -96,14 +96,14 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      * @param acceleration the desired acceleration vector (in physics-space
      * coordinates, not null, unaffected)
      */
-    abstract void setGravity(Vector3f acceleration);
+    abstract public void setGravity(Vector3f acceleration);
 
     /**
      * Alter this body's total mass.
      *
      * @param mass the desired total mass (&ge;0)
      */
-    abstract void setMass(float mass);
+    abstract public void setMass(float mass);
 
     /**
      * Directly relocate this body's center.
@@ -111,5 +111,5 @@ abstract public class PhysicsBody extends PhysicsCollisionObject {
      * @param location the desired location (in physics-space coordinates, not
      * null, unaffected)
      */
-    abstract void setPhysicsLocation(Vector3f location);
+    abstract public void setPhysicsLocation(Vector3f location);
 }
