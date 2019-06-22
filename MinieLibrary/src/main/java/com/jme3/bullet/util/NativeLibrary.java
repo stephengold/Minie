1f20d7fdba1f9a3e6b8a10552bb7dfa4aef7a248
Stephen Gold <sgold@sonic.net>
2019-06-22 14:03:58 -0700

    use Libbulletjme v1.0.84, add NativeLibrary class

---
+++ MinieLibrary/src/main/java/com/jme3/bullet/util/NativeLibrary.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2019 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.bullet.util;
+
+/**
+ * Static interface to the Libbulletjme native library.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class NativeLibrary {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private NativeLibrary() {
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Test whether the native library was built with debugging enabled.
+     *
+     * @return true if Debug buildType, false if Release buildType
+     */
+    native public static boolean isDebug();
+
+    /**
+     * Test whether the native library is using double-precision arithmetic.
+     *
+     * @return true if double-precision, false if single-precision
+     */
+    native public static boolean isDoublePrecision();
+
+    /**
+     * Alter whether the native library will print its startup message.
+     *
+     * @param printFlag true &rarr; print message, false &rarr; no message
+     * (default=true)
+     */
+    native public static void setStartupMessageEnabled(boolean printFlag);
+}
