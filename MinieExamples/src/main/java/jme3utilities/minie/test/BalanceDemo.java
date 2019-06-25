7b0cc7a8bf879d7fdf6e1c70db1b0b8c3d5eff43
sgold <sgold@sgold-h8-1210t>
2019-06-25 12:24:28 -0700

    BalanceDemo: add hotkey hints

--- MinieExamples/src/main/java/jme3utilities/minie/test/BalanceDemo.java
+++ MinieExamples/src/main/java/jme3utilities/minie/test/BalanceDemo.java
@@ -45,6 +45,7 @@ import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.font.Rectangle;
 import com.jme3.input.CameraInput;
 import com.jme3.input.KeyInput;
 import com.jme3.light.AmbientLight;
@@ -158,6 +159,10 @@ public class BalanceDemo extends ActionApplication {
      * root node of the C-G model on which the Control is being tested
      */
     private Node cgModel;
+    /**
+     * GUI node for displaying hotkey help/hints
+     */
+    private Node helpNode;
     /**
      * dump debugging information to System.out
      */
@@ -298,10 +303,21 @@ public class BalanceDemo extends ActionApplication {
         dim.bind("signal orbitLeft", KeyInput.KEY_LEFT);
         dim.bind("signal orbitRight", KeyInput.KEY_RIGHT);
 
+        dim.bind("toggle help", KeyInput.KEY_H);
         dim.bind("toggle meshes", KeyInput.KEY_M);
         dim.bind("toggle pause", KeyInput.KEY_PERIOD);
         dim.bind("toggle physics debug", KeyInput.KEY_SLASH);
         dim.bind("toggle skeleton", KeyInput.KEY_V);
+
+        float x = 10f;
+        float y = cam.getHeight() - 10f;
+        float width = cam.getWidth() - 20f;
+        float height = cam.getHeight() - 20f;
+        Rectangle rectangle = new Rectangle(x, y, width, height);
+
+        float space = 20f;
+        helpNode = HelpUtils.buildNode(dim, rectangle, guiFont, space);
+        guiNode.attachChild(helpNode);
     }
 
     /**
@@ -326,6 +342,10 @@ public class BalanceDemo extends ActionApplication {
                     if (dac.isReady()) {
                         dac.setRagdollMode();
                     }
+                    return;
+
+                case "toggle help":
+                    toggleHelp();
                     return;
 
                 case "toggle meshes":
@@ -824,6 +844,17 @@ public class BalanceDemo extends ActionApplication {
                 break;
             default:
                 throw new IllegalArgumentException(side);
+        }
+    }
+
+    /**
+     * Toggle visibility of the helpNode.
+     */
+    private void toggleHelp() {
+        if (helpNode.getCullHint() == Spatial.CullHint.Always) {
+            helpNode.setCullHint(Spatial.CullHint.Never);
+        } else {
+            helpNode.setCullHint(Spatial.CullHint.Always);
         }
     }
 
