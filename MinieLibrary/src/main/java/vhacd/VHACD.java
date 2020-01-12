c4bc8eab7f9c5759fd103fd734e2f00664e722d3
Stephen Gold <sgold@sonic.net>
2020-01-12 11:02:20 -0800

    vhacd: organize imports, move license to top of file, and re-format code

--- MinieLibrary/src/main/java/vhacd/VHACD.java
+++ MinieLibrary/src/main/java/vhacd/VHACD.java
@@ -1,10 +1,3 @@
-package vhacd;
-
-import vhacd.vhacd_native.VhacdLibrary;
-import vhacd.vhacd_native.VHACDNativeParameters;
-import vhacd.vhacd_native.VHACDNativeResults;
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
 /*
 Copyright (c) 2016, Riccardo Balbo
 All rights reserved.
@@ -18,30 +11,36 @@ Redistribution and use in source and binary forms, with or without modification,
 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
+package vhacd;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import vhacd.vhacd_native.VHACDNativeResults;
+import vhacd.vhacd_native.VhacdLibrary;
 
-public class VHACD{
-	public static VHACDResults compute(float positions[], int indexes[], VHACDParameters params) {
-		int pos_buffer_l=positions.length;
-		int index_buffer_l=indexes.length;
+public class VHACD {
 
-        FloatBuffer b_pos=FloatBuffer.wrap(positions);
-        IntBuffer b_ind=IntBuffer.wrap(indexes);
+    public static VHACDResults compute(float positions[], int indexes[], VHACDParameters params) {
+        int pos_buffer_l = positions.length;
+        int index_buffer_l = indexes.length;
+
+        FloatBuffer b_pos = FloatBuffer.wrap(positions);
+        IntBuffer b_ind = IntBuffer.wrap(indexes);
         b_pos.rewind();
         b_ind.rewind();
-        
-		VHACDNativeResults r=VhacdLibrary.INSTANCE.compute(
-            b_pos,
-            pos_buffer_l,
-            b_ind,
-            index_buffer_l,
-            params,
-            params.getDebugEnabled()?(byte)1:(byte)0);  
-             
-		VHACDResults result=new VHACDResults(r);       
+
+        VHACDNativeResults r = VhacdLibrary.INSTANCE.compute(
+                b_pos,
+                pos_buffer_l,
+                b_ind,
+                index_buffer_l,
+                params,
+                params.getDebugEnabled() ? (byte) 1 : (byte) 0);
+
+        VHACDResults result = new VHACDResults(r);
         VhacdLibrary.INSTANCE.release(r);
-        
-		return result;
-	}
+
+        return result;
+    }
 }
--- MinieLibrary/src/main/java/vhacd/VHACDResults.java
+++ MinieLibrary/src/main/java/vhacd/VHACDResults.java
@@ -1,11 +1,3 @@
-package vhacd;
-
-import java.util.ArrayList;
-
-import com.sun.jna.Pointer;
-
-import vhacd.vhacd_native.VHACDNativeConvexHull;
-import vhacd.vhacd_native.VHACDNativeResults;
 /*
 Copyright (c) 2016, Riccardo Balbo
 All rights reserved.
@@ -19,26 +11,33 @@ Redistribution and use in source and binary forms, with or without modification,
 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
+package vhacd;
 
-public class VHACDResults extends ArrayList<VHACDHull>{
-	private static final long serialVersionUID=1L;
+import com.sun.jna.Pointer;
+import java.util.ArrayList;
+import vhacd.vhacd_native.VHACDNativeConvexHull;
+import vhacd.vhacd_native.VHACDNativeResults;
 
-	protected VHACDResults(VHACDNativeResults nat){
-		int nhulls=nat.n_hulls;
-		VHACDNativeConvexHull[] hulls=(VHACDNativeConvexHull[])nat.hulls.toArray(nhulls);
-	
-		for(int i=0;i<nhulls;i++){
-			VHACDNativeConvexHull nhull=hulls[i];
-			
-		    Pointer p_positions=nhull.positions.getPointer();            
-			float positions[]=p_positions.getFloatArray(0,nhull.n_positions*3);			
-            
-		    Pointer p_indexes=nhull.indexes.getPointer();            
-			int indexes[]=p_indexes.getIntArray(0,nhull.n_indexes*3);
-			
-			VHACDHull hull=new VHACDHull(positions,indexes);
-			add(hull);
-		}
-	}
+public class VHACDResults extends ArrayList<VHACDHull> {
+
+    private static final long serialVersionUID = 1L;
+
+    protected VHACDResults(VHACDNativeResults nat) {
+        int nhulls = nat.n_hulls;
+        VHACDNativeConvexHull[] hulls = (VHACDNativeConvexHull[]) nat.hulls.toArray(nhulls);
+
+        for (int i = 0; i < nhulls; i++) {
+            VHACDNativeConvexHull nhull = hulls[i];
+
+            Pointer p_positions = nhull.positions.getPointer();
+            float positions[] = p_positions.getFloatArray(0, nhull.n_positions * 3);
+
+            Pointer p_indexes = nhull.indexes.getPointer();
+            int indexes[] = p_indexes.getIntArray(0, nhull.n_indexes * 3);
+
+            VHACDHull hull = new VHACDHull(positions, indexes);
+            add(hull);
+        }
+    }
 }
