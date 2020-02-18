57951a897111e974b00ce2a5c8f8a06753f638e2
Stephen Gold <sgold@sonic.net>
2020-02-17 21:24:49 -0800

    TestIssue3: count ticks after the jump()

--- MinieExamples/src/main/java/jme3utilities/minie/test/issue/TestIssue3.java
+++ MinieExamples/src/main/java/jme3utilities/minie/test/issue/TestIssue3.java
@@ -28,11 +28,14 @@ package jme3utilities.minie.test.issue;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
 import com.jme3.bullet.objects.PhysicsCharacter;
 import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.bullet.util.NativeLibrary;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
@@ -45,9 +48,12 @@ import jme3utilities.math.MyBuffer;
 import jme3utilities.mesh.RectangleMesh;
 import jme3utilities.minie.PhysicsDumper;
 
-public class TestIssue3 extends SimpleApplication implements ActionListener {
+public class TestIssue3 extends SimpleApplication
+        implements ActionListener, PhysicsTickListener {
 
+    private int tickCount = 0;
     private PhysicsCharacter character;
+    private PhysicsSpace physicsSpace;
 
     public static void main(String[] args) {
         TestIssue3 app = new TestIssue3();
@@ -62,6 +68,9 @@ public class TestIssue3 extends SimpleApplication implements ActionListener {
         BulletAppState bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
         bulletAppState.setDebugEnabled(true);
+        physicsSpace = bulletAppState.getPhysicsSpace();
+
+        assert NativeLibrary.isDebug() : "This test requires a Debug library.";
 
         Mesh mesh = new RectangleMesh(-999f, 999f, -999f, 999f, 1f);
         FloatBuffer buffer = mesh.getFloatBuffer(VertexBuffer.Type.Position);
@@ -69,21 +78,38 @@ public class TestIssue3 extends SimpleApplication implements ActionListener {
         MyBuffer.rotate(buffer, 0, buffer.limit(), rot);
         CollisionShape meshShape = new MeshCollisionShape(mesh);
         PhysicsRigidBody staticBody = new PhysicsRigidBody(meshShape, 0f);
-        bulletAppState.getPhysicsSpace().add(staticBody);
+        physicsSpace.add(staticBody);
 
         CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1f, 3f);
         character = new PhysicsCharacter(capsuleShape, 0.05f);
         character.setGravity(120f);
         character.setPhysicsLocation(new Vector3f(0f, 0.8f, 0f));
-        bulletAppState.getPhysicsSpace().add(character);
+        physicsSpace.add(character);
 
         new PhysicsDumper().dump(bulletAppState);
     }
 
     @Override
-    public void onAction(String binding, boolean value, float tpf) {
-        if (binding.equals("Jump")) {
+    public void onAction(String binding, boolean ongoing, float tpf) {
+        if (ongoing && binding.equals("Jump")) {
+            if (physicsSpace.countTickListeners() == 0) {
+                physicsSpace.addTickListener(this);
+            }
             character.jump();
         }
+    }
+
+    @Override
+    public void prePhysicsTick(PhysicsSpace space, float timeStep) {
+        ++tickCount;
+        System.out.println("tick #" + Integer.toString(tickCount));
+        if (tickCount >= 5) {
+            System.out.flush();
+        }
+    }
+
+    @Override
+    public void physicsTick(PhysicsSpace space, float timeStep) {
+        // do nothing
     }
 }
