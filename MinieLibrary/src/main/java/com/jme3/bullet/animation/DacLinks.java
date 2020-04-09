d2ed290a6d192629319428b9e4f0c7a2dd5fddc5
Stephen Gold <sgold@sonic.net>
2020-04-09 00:58:38 -0700

    DacLinks: catch attempts to add a 2nd DAC to a Spatial

--- MinieLibrary/src/main/java/com/jme3/bullet/animation/DacLinks.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/animation/DacLinks.java
@@ -623,6 +623,13 @@ public class DacLinks
     @Override
     protected void createSpatialData(Spatial spatial) {
         RagUtils.validate(spatial);
+        int numDacs
+                = MySpatial.countControls(spatial, DynamicAnimControl.class);
+        if (numDacs > 1) {
+            logger3.log(Level.WARNING, "Added a DynamicAnimControl to a model "
+                    + "that already contains {0}.",
+                    numDacs - 1);
+        }
 
         SkeletonControl skeletonControl
                 = spatial.getControl(SkeletonControl.class);
