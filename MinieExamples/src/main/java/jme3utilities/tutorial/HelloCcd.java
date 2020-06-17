7b8920c2e5d07f3f0154ca8b43af5cf8ce9f70f7
Stephen Gold <sgold@sonic.net>
2020-06-17 13:54:12 -0700

    MinieExamples: add the HelloCcd tutorial app

---
+++ MinieExamples/src/main/java/jme3utilities/tutorial/HelloCcd.java
@@ -0,0 +1,107 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.CylinderCollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.math.Vector3f;
+
+/**
+ * A simple example of continous collision detection (CCD).
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HelloCcd extends SimpleApplication {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the HelloCcd application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        HelloCcd application = new HelloCcd();
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        // Set up Bullet physics and create a physics space.
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        PhysicsSpace physicsSpace = bulletAppState.getPhysicsSpace();
+
+        // Enable debug visualization to reveal what occurs in physics space.
+        bulletAppState.setDebugEnabled(true);
+
+        // For clarity, simulate at 1/10th normal speed.
+        bulletAppState.setSpeed(0.1f);
+
+        // Increase gravity to make the balls fall faster.
+        physicsSpace.setGravity(new Vector3f(0f, -100f, 0f));
+
+        // Create a CollisionShape for balls.
+        float ballRadius = 0.1f;
+        CollisionShape ballShape = new SphereCollisionShape(ballRadius);
+
+        // Create 2 dynamic balls, one with CCD and one without,
+        // and add them to the space.
+        float mass = 1f;
+        PhysicsRigidBody ccdBall = new PhysicsRigidBody(ballShape, mass);
+        physicsSpace.add(ccdBall);
+        ccdBall.setCcdMotionThreshold(ballRadius);
+        ccdBall.setCcdSweptSphereRadius(ballRadius);
+        ccdBall.setPhysicsLocation(new Vector3f(-1f, 4f, 0f));
+
+        PhysicsRigidBody controlBall = new PhysicsRigidBody(ballShape, mass);
+        physicsSpace.add(controlBall);
+        controlBall.setPhysicsLocation(new Vector3f(1f, 4f, 0f));
+
+        // Create a thin, static disc and add it to the space.
+        float discRadius = 2f;
+        float discThickness = 0.05f;
+        CollisionShape discShape = new CylinderCollisionShape(discRadius,
+                discThickness, PhysicsSpace.AXIS_Y);
+        PhysicsRigidBody disc
+                = new PhysicsRigidBody(discShape, PhysicsBody.massForStatic);
+        physicsSpace.add(disc);
+
+        // Minie's BulletAppState simulates the dynamics...
+    }
+}
