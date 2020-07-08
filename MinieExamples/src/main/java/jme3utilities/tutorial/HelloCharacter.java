9645a2569d433efb346323c80aae1e7ccdba5623
Stephen Gold <sgold@sonic.net>
2020-07-07 20:50:41 -0700

    MinieExamples: add the HelloCharacter tutorial app

---
+++ MinieExamples/src/main/java/jme3utilities/tutorial/HelloCharacter.java
@@ -0,0 +1,245 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.bullet.collision.shapes.Box2dShape;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.infos.DebugMeshNormals;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.debug.DebugInitListener;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsCharacter;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.bullet.util.DebugShapeFactory;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.shadow.EdgeFilteringMode;
+import com.jme3.system.AppSettings;
+
+/**
+ * A simple example of character physics.
+ *
+ * Builds upon HelloCustomDebug.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HelloCharacter
+        extends SimpleApplication
+        implements PhysicsTickListener {
+    // *************************************************************************
+    // fields
+
+    private PhysicsCharacter character;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the HelloCharacter application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        HelloCharacter application = new HelloCharacter();
+
+        // Enable gamma correction for accurate lighting.
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        settings.setGammaCorrection(true);
+        application.setSettings(settings);
+
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        PhysicsSpace physicsSpace = configurePhysics();
+
+        // Create a character with a capsule shape.
+        float characterRadius = 0.5f;
+        float characterHeight = 1f;
+        CapsuleCollisionShape characterShape
+                = new CapsuleCollisionShape(characterRadius, characterHeight);
+        float stepHeight = 0.3f;
+        character = new PhysicsCharacter(characterShape, stepHeight);
+        physicsSpace.addCollisionObject(character);
+
+        // Create a static square to represent the ground.
+        float halfExtent = 4f;
+        Box2dShape boxShape = new Box2dShape(halfExtent);
+        PhysicsRigidBody ground
+                = new RigidBodyControl(boxShape, PhysicsBody.massForStatic);
+        physicsSpace.addCollisionObject(ground);
+
+        // Rotate it 90 degrees to a horizontal orientation.
+        Matrix3f rotate90 = new Matrix3f();
+        rotate90.fromAngleAxis(-FastMath.HALF_PI, Vector3f.UNIT_X);
+        ground.setPhysicsRotation(rotate90);
+
+        // Lower the ground level by 2 physics-space units.
+        ground.setPhysicsLocation(new Vector3f(0f, -2f, 0f));
+
+        // Customize the debug visualization of each object.
+        Material redMaterial = createLitMaterial(1f, 0f, 0f);
+        character.setDebugMaterial(redMaterial);
+        character.setDebugMeshNormals(DebugMeshNormals.Smooth);
+        character.setDebugMeshResolution(DebugShapeFactory.highResolution);
+
+        Material greenMaterial = createLitMaterial(0f, 1f, 0f);
+        ground.setDebugMaterial(greenMaterial);
+        character.setDebugMeshNormals(DebugMeshNormals.Facet);
+
+        // Minie's BulletAppState simulates the dynamics...
+    }
+    // *************************************************************************
+    // PhysicsTickListener methods
+
+    /**
+     * Callback from Bullet, invoked just before the physics is stepped.
+     *
+     * @param space the space that is about to be stepped (not null)
+     * @param timeStep the time per physics step (in seconds, &ge;0)
+     */
+    @Override
+    public void prePhysicsTick(PhysicsSpace space, float timeStep) {
+        // If the character is touching the ground, cause it to jump.
+        if (character.onGround()) {
+            character.jump();
+        }
+    }
+
+    /**
+     * Callback from Bullet, invoked just after the physics has been stepped.
+     *
+     * @param space the space that was just stepped (not null)
+     * @param timeStep the time per physics step (in seconds, &ge;0)
+     */
+    @Override
+    public void physicsTick(PhysicsSpace space, float timeStep) {
+        // do nothing
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add lighting and shadows to the specified scene.
+     */
+    private void addLighting(Spatial scene) {
+        ColorRGBA ambientColor = new ColorRGBA(0.02f, 0.02f, 0.02f, 1f);
+        AmbientLight ambient = new AmbientLight(ambientColor);
+        scene.addLight(ambient);
+        ambient.setName("ambient");
+
+        ColorRGBA directColor = new ColorRGBA(0.2f, 0.2f, 0.2f, 1f);
+        Vector3f direction = new Vector3f(-7f, -3f, -5f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(direction, directColor);
+        scene.addLight(sun);
+        sun.setName("sun");
+
+        // Render shadows based on the directional light.
+        viewPort.clearProcessors();
+        int shadowMapSize = 2_048; // in pixels
+        int numSplits = 3;
+        DirectionalLightShadowRenderer dlsr
+                = new DirectionalLightShadowRenderer(assetManager,
+                        shadowMapSize, numSplits);
+        dlsr.setEdgeFilteringMode(EdgeFilteringMode.PCFPOISSON);
+        dlsr.setEdgesThickness(5);
+        dlsr.setLight(sun);
+        dlsr.setShadowIntensity(0.5f);
+        viewPort.addProcessor(dlsr);
+
+        // Set the viewport's background color to light blue.
+        ColorRGBA skyColor = new ColorRGBA(0.1f, 0.2f, 0.4f, 1f);
+        viewPort.setBackgroundColor(skyColor);
+    }
+
+    /**
+     * Configure physics during startup.
+     */
+    private PhysicsSpace configurePhysics() {
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+
+        // Enable debug visualization to reveal what occurs in physics space.
+        bulletAppState.setDebugEnabled(true);
+
+        // Add lighting and shadows to the debug scene.
+        bulletAppState.setDebugInitListener(new DebugInitListener() {
+            @Override
+            public void bulletDebugInit(Node physicsDebugRootNode) {
+                addLighting(physicsDebugRootNode);
+            }
+        });
+        bulletAppState.setDebugShadowMode(
+                RenderQueue.ShadowMode.CastAndReceive);
+
+        PhysicsSpace result = bulletAppState.getPhysicsSpace();
+
+        // Activate the PhysicsTickListener interface.
+        result.addTickListener(this);
+
+        return result;
+    }
+
+    /**
+     * Create a single-sided lit material with the specified reflectivities.
+     *
+     * @param red the desired reflectivity for red light (&ge;0, &le;1)
+     * @param green the desired reflectivity for green light (&ge;0, &le;1)
+     * @param blue the desired reflectivity for blue light (&ge;0, &le;1)
+     * @return a new instance (not null)
+     */
+    private Material createLitMaterial(float red, float green, float blue) {
+        Material result = new Material(assetManager, Materials.LIGHTING);
+        result.setBoolean("UseMaterialColors", true);
+
+        float opacity = 1f;
+        result.setColor("Ambient", new ColorRGBA(red, green, blue, opacity));
+        result.setColor("Diffuse", new ColorRGBA(red, green, blue, opacity));
+
+        return result;
+    }
+}
