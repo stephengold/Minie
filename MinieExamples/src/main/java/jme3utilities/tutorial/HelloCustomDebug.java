c7aee4c8fd50dcb995a0af070fc8470858889876
Stephen Gold <sgold@sonic.net>
2020-07-07 20:39:38 -0700

    MinieExamples: add the HelloCustomDebug tutorial app

---
+++ MinieExamples/src/main/java/jme3utilities/tutorial/HelloCustomDebug.java
@@ -0,0 +1,159 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.collision.shapes.infos.DebugMeshNormals;
+import com.jme3.bullet.debug.DebugInitListener;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.bullet.util.DebugShapeFactory;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.system.AppSettings;
+
+/**
+ * A simple example that demonstrates customization of debug materials, debug
+ * meshes, and lighting.
+ *
+ * Builds upon HelloRbc.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HelloCustomDebug extends SimpleApplication {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the HelloCustomDebug application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        HelloCustomDebug application = new HelloCustomDebug();
+
+        // Enable gamma correction for accurate lighting.
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        settings.setGammaCorrection(true);
+        application.setSettings(settings);
+
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        // Set up Bullet physics and create a physics space.
+        PhysicsSpace physicsSpace = configurePhysics();
+
+        // Create a material and CollisionShape for balls.
+        Material ballMaterial = new Material(assetManager, Materials.LIGHTING);
+        float ballRadius = 1f;
+        CollisionShape ballShape = new SphereCollisionShape(ballRadius);
+
+        // Create rigid bodies for a dynamic ball and a static ball.
+        float mass = 2f;
+        PhysicsRigidBody dynaBall = new PhysicsRigidBody(ballShape, mass);
+
+        PhysicsRigidBody statBall
+                = new PhysicsRigidBody(ballShape, PhysicsBody.massForStatic);
+
+        // Add the bodies to the physics space.
+        physicsSpace.add(dynaBall);
+        physicsSpace.add(statBall);
+
+        // Position the balls in physics space.
+        dynaBall.setPhysicsLocation(new Vector3f(0f, 4f, 0f));
+        statBall.setPhysicsLocation(new Vector3f(0.1f, 0f, 0f));
+
+        // Customize the debug visualization of each object.
+        dynaBall.setDebugMaterial(ballMaterial);
+        dynaBall.setDebugMeshNormals(DebugMeshNormals.Sphere);
+        dynaBall.setDebugMeshResolution(DebugShapeFactory.highResolution);
+
+        statBall.setDebugMaterial(ballMaterial);
+        statBall.setDebugMeshNormals(DebugMeshNormals.Sphere);
+        statBall.setDebugMeshResolution(DebugShapeFactory.highResolution);
+
+        // Minie's BulletAppState simulates the dynamics...
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add lighting to the specified scene.
+     */
+    private void addLighting(Spatial scene) {
+        // Light the scene with ambient and directional lights.
+        ColorRGBA ambientColor = new ColorRGBA(0.02f, 0.02f, 0.02f, 1f);
+        AmbientLight ambient = new AmbientLight(ambientColor);
+        scene.addLight(ambient);
+        ambient.setName("ambient");
+
+        ColorRGBA directColor = new ColorRGBA(0.2f, 0.2f, 0.2f, 1f);
+        Vector3f direction = new Vector3f(-7f, -3f, -5f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(direction, directColor);
+        scene.addLight(sun);
+        sun.setName("sun");
+    }
+
+    /**
+     * Configure physics during startup.
+     */
+    private PhysicsSpace configurePhysics() {
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.setDebugEnabled(true);
+
+        // Add lighting to the debug scene.
+        bulletAppState.setDebugInitListener(new DebugInitListener() {
+            @Override
+            public void bulletDebugInit(Node physicsDebugRootNode) {
+                addLighting(physicsDebugRootNode);
+            }
+        });
+
+        PhysicsSpace result = bulletAppState.getPhysicsSpace();
+
+        return result;
+    }
+}
