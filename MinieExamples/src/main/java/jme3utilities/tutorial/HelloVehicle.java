27a446993b2ed318132c3260b9f1e910e7448d3f
Stephen Gold <sgold@sonic.net>
2020-07-10 19:23:07 -0700

    MinieExamples: add the HelloVehicle tutorial app

---
+++ MinieExamples/src/main/java/jme3utilities/tutorial/HelloVehicle.java
@@ -0,0 +1,187 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.TextureKey;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.HullCollisionShape;
+import com.jme3.bullet.collision.shapes.PlaneCollisionShape;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.bullet.objects.PhysicsVehicle;
+import com.jme3.bullet.util.PlaneDmiListener;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.FastMath;
+import com.jme3.math.Plane;
+import com.jme3.math.Vector3f;
+import com.jme3.texture.Texture;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * A simple example of vehicle physics.
+ *
+ * Builds upon HelloStaticBody.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HelloVehicle extends SimpleApplication {
+    // *************************************************************************
+    // fields
+
+    private PhysicsVehicle vehicle;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the HelloVehicle application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        HelloVehicle application = new HelloVehicle();
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        PhysicsSpace physicsSpace = configurePhysics();
+
+        // Create a wedge-shaped vehicle with a low center of gravity.
+        // The local forward direction is +Z.
+        float noseZ = 1.4f;           // offset from chassis center
+        float spoilerY = 0.5f;        // offset from chassis center
+        float tailZ = -0.7f;          // offset from chassis center
+        float undercarriageY = -0.1f; // offset from chassis center
+        float halfWidth = 0.4f;
+        Collection<Vector3f> cornerLocations = new ArrayList<>(6);
+        cornerLocations.add(new Vector3f(+halfWidth, undercarriageY, noseZ));
+        cornerLocations.add(new Vector3f(-halfWidth, undercarriageY, noseZ));
+        cornerLocations.add(new Vector3f(+halfWidth, undercarriageY, tailZ));
+        cornerLocations.add(new Vector3f(-halfWidth, undercarriageY, tailZ));
+        cornerLocations.add(new Vector3f(+halfWidth, spoilerY, tailZ));
+        cornerLocations.add(new Vector3f(-halfWidth, spoilerY, tailZ));
+        HullCollisionShape wedgeShape
+                = new HullCollisionShape(cornerLocations);
+        float mass = 5f;
+        vehicle = new PhysicsVehicle(wedgeShape, mass);
+        vehicle.setSuspensionCompression(6f); // default=0.83
+        vehicle.setSuspensionDamping(7f); // default=0.88
+        vehicle.setSuspensionStiffness(150f); // default=5.88
+
+        // Add 4 wheels, 2 in the front (for steering) and 2 in the rear.
+        boolean front = true;
+        boolean rear = false;
+        float frontAxisZ = 0.7f * noseZ; // offset from chassis center
+        float rearAxisZ = 0.8f * tailZ; // offset from chassis center
+        float radius = 0.3f; // of each tire
+        float restLength = 0.2f; // of the suspension
+        float xOffset = 0.9f * halfWidth;
+        Vector3f axleDirection = new Vector3f(-1f, 0f, 0f);
+        Vector3f suspensionDirection = new Vector3f(0f, -1f, 0f);
+        vehicle.addWheel(new Vector3f(-xOffset, 0f, frontAxisZ),
+                suspensionDirection, axleDirection, restLength, radius, front);
+        vehicle.addWheel(new Vector3f(xOffset, 0f, frontAxisZ),
+                suspensionDirection, axleDirection, restLength, radius, front);
+        vehicle.addWheel(new Vector3f(-xOffset, 0f, rearAxisZ),
+                suspensionDirection, axleDirection, restLength, radius, rear);
+        vehicle.addWheel(new Vector3f(xOffset, 0f, rearAxisZ),
+                suspensionDirection, axleDirection, restLength, radius, rear);
+
+        physicsSpace.addCollisionObject(vehicle);
+
+        // Apply a steering angle of 6 degrees left (to the front wheels).
+        vehicle.steer(FastMath.PI / 30f);
+
+        // Apply a constant acceleration (to the chassis).
+        vehicle.accelerate(1f);
+
+        // Add a plane to represent the ground.
+        float y = -radius - 0.35f;
+        addPlane(y, physicsSpace);
+
+        // Enable anisotropic filtering for textures, to reduce blurring.
+        renderer.setDefaultAnisotropicFilter(8);
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add a horizontal plane body to the specified PhysicsSpace.
+     *
+     * @param y (the desired elevation, in physics-space coordinates)
+     * @param physicsSpace (not null)
+     */
+    private void addPlane(float y, PhysicsSpace physicsSpace) {
+        Plane plane = new Plane(Vector3f.UNIT_Y, y);
+        PlaneCollisionShape shape = new PlaneCollisionShape(plane);
+        PhysicsRigidBody body
+                = new PhysicsRigidBody(shape, PhysicsBody.massForStatic);
+
+        // Generate a repeating tile texture.
+        String assetPath = "Textures/greenTile.png";
+        boolean flipY = false;
+        TextureKey key = new TextureKey(assetPath, flipY);
+        boolean generateMips = true;
+        key.setGenerateMips(generateMips);
+        Texture texture = assetManager.loadTexture(key);
+        texture.setMinFilter(Texture.MinFilter.Trilinear);
+        texture.setWrap(Texture.WrapMode.Repeat);
+
+        // Apply a tiled, unshaded debug material to the body.
+        Material material = new Material(assetManager, Materials.UNSHADED);
+        material.setTexture("ColorMap", texture);
+        body.setDebugMaterial(material);
+
+        // Generate texture coordinates during debug-mesh initialization.
+        float tileSize = 1f;
+        PlaneDmiListener planeDmiListener = new PlaneDmiListener(tileSize);
+        body.setDebugMeshInitListener(planeDmiListener);
+
+        physicsSpace.addCollisionObject(body);
+    }
+
+    /**
+     * Configure physics during startup.
+     */
+    private PhysicsSpace configurePhysics() {
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.setDebugEnabled(true);
+        PhysicsSpace result = bulletAppState.getPhysicsSpace();
+
+        return result;
+    }
+}
