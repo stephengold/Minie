153445e2890792b9c7852e63dabb7e3af1ad8560
Stephen Gold <sgold@sonic.net>
2021-01-23 17:46:27 -0800

    HelloPoi: no longer necessry to override bucket for PointVisualizer

--- MinieExamples/src/main/java/jme3utilities/tutorial/HelloPoi.java
+++ MinieExamples/src/main/java/jme3utilities/tutorial/HelloPoi.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2020, Stephen Gold
+ Copyright (c) 2020-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -147,7 +147,6 @@ public class HelloPoi
                 ColorRGBA.Yellow, "cross");
         rootNode.attachChild(poiIndicator);
         poiIndicator.setDepthTest(true);
-        poiIndicator.setQueueBucket(RenderQueue.Bucket.Translucent);
 
         // Add a static heightmap to represent the ground.
         addTerrain(physicsSpace);
