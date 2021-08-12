3833ac5ae0231f0eb3410901ced19181cad70dec
Stephen Gold <sgold@sonic.net>
2021-08-12 11:40:34 -0700

    MinieExamples: add TowerPerformance for performance testing

---
+++ MinieExamples/src/main/java/jme3test/batching/BombControl.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2009-2021 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3test.batching;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.bullet.collision.PhysicsCollisionEvent;
+import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.objects.PhysicsGhostObject;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh.Type;
+import com.jme3.effect.shapes.EmitterSphereShape;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * @author normenhansen
+ */
+class BombControl
+        extends RigidBodyControl
+        implements PhysicsCollisionListener, PhysicsTickListener {
+    // *************************************************************************
+    // constants and loggers
+
+    final private static float explosionRadius = 10f;
+    final private static float forceFactor = 1f;
+    final private static float fxTime = 0.5f;
+    final private static float maxTime = 4f;
+    // *************************************************************************
+    // fields
+
+    private float curTime = -1.0f;
+    private float timer;
+    private ParticleEmitter effect;
+    private PhysicsGhostObject ghostObject;
+    final private Vector3f vector = new Vector3f();
+    final private Vector3f vector2 = new Vector3f();
+    // *************************************************************************
+    // constructors
+
+    BombControl(AssetManager manager, CollisionShape shape, float mass) {
+        super(shape, mass);
+        createGhostObject();
+        prepareEffect(manager);
+    }
+    // *************************************************************************
+    // PhysicsCollisionListener methods
+
+    @Override
+    public void collision(PhysicsCollisionEvent event) {
+        PhysicsSpace space = getPhysicsSpace();
+        if (space == null) {
+            return;
+        }
+        if (event.getObjectA() == this || event.getObjectB() == this) {
+            space.add(ghostObject);
+            ghostObject.setPhysicsLocation(getPhysicsLocation(vector));
+            space.addTickListener(this);
+            Spatial spatial = getSpatial();
+            if (effect != null && spatial.getParent() != null) {
+                curTime = 0;
+                effect.setLocalTranslation(spatial.getLocalTranslation());
+                spatial.getParent().attachChild(effect);
+                effect.emitAllParticles();
+            }
+            space.remove(this);
+            spatial.removeFromParent();
+        }
+    }
+    // *************************************************************************
+    // PhysicsTickListener methods
+
+    @Override
+    public void physicsTick(PhysicsSpace space, float f) {
+        //get all overlapping objects and apply impulse to them
+        for (Iterator<PhysicsCollisionObject> it = ghostObject.getOverlappingObjects().iterator(); it.hasNext();) {
+            PhysicsCollisionObject physicsCollisionObject = it.next();
+            if (physicsCollisionObject instanceof PhysicsRigidBody) {
+                PhysicsRigidBody rBody = (PhysicsRigidBody) physicsCollisionObject;
+                rBody.getPhysicsLocation(vector2);
+                vector2.subtractLocal(vector);
+                float force = explosionRadius - vector2.length();
+                force *= forceFactor;
+                force = force > 0 ? force : 0;
+                vector2.normalizeLocal();
+                vector2.multLocal(force);
+                ((PhysicsRigidBody) physicsCollisionObject).applyImpulse(vector2, Vector3f.ZERO);
+            }
+        }
+        space.removeTickListener(this);
+        space.remove(ghostObject);
+    }
+
+    @Override
+    public void prePhysicsTick(PhysicsSpace space, float f) {
+        space.removeCollisionListener(this);
+    }
+    // *************************************************************************
+    // RigidBodyControl methods
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        throw new UnsupportedOperationException("Reading not supported.");
+    }
+
+    @Override
+    public void setPhysicsSpace(PhysicsSpace space) {
+        super.setPhysicsSpace(space);
+        if (space != null) {
+            space.addCollisionListener(this);
+        }
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        throw new UnsupportedOperationException("Saving not supported.");
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+        boolean enabled = isEnabled();
+        if (enabled) {
+            timer += tpf;
+            if (timer > maxTime) {
+                Spatial spatial = getSpatial();
+                if (spatial.getParent() != null) {
+                    PhysicsSpace space = getPhysicsSpace();
+                    space.removeCollisionListener(this);
+                    space.remove(this);
+                    spatial.removeFromParent();
+                }
+            }
+        }
+        if (enabled && curTime >= 0) {
+            curTime += tpf;
+            if (curTime > fxTime) {
+                curTime = -1;
+                effect.removeFromParent();
+            }
+        }
+    }
+    // *************************************************************************
+    // private methods
+
+    private void prepareEffect(AssetManager assetManager) {
+        int COUNT_FACTOR = 1;
+        float COUNT_FACTOR_F = 1f;
+        effect = new ParticleEmitter("Flame", Type.Triangle, 32 * COUNT_FACTOR);
+        effect.setSelectRandomImage(true);
+        effect.setStartColor(new ColorRGBA(1f, 0.4f, 0.05f, (1f / COUNT_FACTOR_F)));
+        effect.setEndColor(new ColorRGBA(.4f, .22f, .12f, 0f));
+        effect.setStartSize(1.3f);
+        effect.setEndSize(2f);
+        effect.setShape(new EmitterSphereShape(Vector3f.ZERO, 1f));
+        effect.setParticlesPerSec(0);
+        effect.setGravity(0, -5f, 0);
+        effect.setLowLife(.4f);
+        effect.setHighLife(.5f);
+        effect.getParticleInfluencer()
+                .setInitialVelocity(new Vector3f(0, 7, 0));
+        effect.getParticleInfluencer().setVelocityVariation(1f);
+        effect.setImagesX(2);
+        effect.setImagesY(2);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        mat.setTexture("Texture", assetManager.loadTexture("Effects/Explosion/flame.png"));
+        effect.setMaterial(mat);
+    }
+
+    private void createGhostObject() {
+        ghostObject = new PhysicsGhostObject(new SphereCollisionShape(explosionRadius));
+    }
+}
---
+++ MinieExamples/src/main/java/jme3test/batching/TowerPerformance.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright (c) 2009-2021 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3test.batching;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.TextureKey;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.util.NativeLibrary;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.BatchNode;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.shadow.CompareMode;
+import com.jme3.shadow.DirectionalLightShadowFilter;
+import com.jme3.shadow.EdgeFilteringMode;
+import com.jme3.system.AppSettings;
+import com.jme3.texture.Texture;
+import com.jme3.texture.Texture.WrapMode;
+import java.util.concurrent.Callable;
+import jme3utilities.Heart;
+import jme3utilities.math.MyMath;
+
+/**
+ * Based on TestBatchNodeTower by double1984 and atom.
+ */
+public class TowerPerformance
+        extends SimpleApplication
+        implements PhysicsTickListener {
+    // *************************************************************************
+    // constants and loggers
+
+    final private static float brickDepth = 0.25f;
+    final private static float brickHeight = 0.25f;
+    final private static float brickWidth = 0.75f;
+    final private static float bulletRadius = 0.4f;
+    final private static float radius = 3f;
+    final private static int brickLayers = 30;
+    final private static int bricksPerLayer = 8;
+    // *************************************************************************
+    // fields
+
+    final private BatchNode batchNode = new BatchNode("batch Node");
+    private Box brick;
+    private BulletAppState bulletAppState;
+    private float angleDegrees;
+    private int numSteps;
+    private long physicsNs;
+    private long preTickNs;
+    private Material mat;
+    private Material mat2;
+    private Material mat3;
+    private Sphere bullet;
+    private SphereCollisionShape bulletCollisionShape;
+    // *************************************************************************
+    // new methods exposed
+
+    public static void main(String args[]) {
+        TowerPerformance f = new TowerPerformance();
+        AppSettings s = new AppSettings(true);
+        s.setAudioRenderer(null);
+        f.setSettings(s);
+        f.start();
+    }
+    // *************************************************************************
+    // PhysicsTickListener methods
+
+    @Override
+    public void physicsTick(PhysicsSpace space, float timeStep) {
+        physicsNs += System.nanoTime() - preTickNs;
+        ++numSteps;
+        if ((numSteps % 50) == 0) {
+            float millisPerStep = (physicsNs * 1e-6f) / 50;
+            System.out.printf("millisPerStep = %.3f%n", millisPerStep);
+            physicsNs = 0L;
+        }
+
+        if (numSteps == 200) {
+            enqueue(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    shoot();
+                    return null;
+                }
+            });
+
+        } else if (numSteps == 600) {
+            enqueue(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    stop();
+                    return null;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void prePhysicsTick(PhysicsSpace space, float timeStep) {
+        preTickNs = System.nanoTime();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    @Override
+    public void simpleInitApp() {
+        if (Heart.areAssertionsEnabled()) {
+            System.out.println("Warning: assertions are enabled.");
+        }
+        if (NativeLibrary.isDebug()) {
+            System.out.println("Warning: using a Debug native library.");
+        }
+
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        getPhysicsSpace().addTickListener(this);
+
+        bullet = new Sphere(32, 32, bulletRadius, true, false);
+        bullet.setTextureMode(Sphere.TextureMode.Projected);
+        bulletCollisionShape = new SphereCollisionShape(bulletRadius);
+
+        brick = new Box(brickWidth, brickHeight, brickDepth);
+        brick.scaleTextureCoordinates(new Vector2f(1f, 0.5f));
+        initMaterial();
+        initTower();
+        initFloor();
+
+        cam.setLocation(new Vector3f(0f, 25f, 8f));
+        cam.lookAt(Vector3f.ZERO, new Vector3f(0f, 1f, 0f));
+        cam.setFrustumFar(80f);
+        flyCam.setEnabled(false);
+
+        rootNode.setShadowMode(RenderQueue.ShadowMode.Off);
+        batchNode.batch();
+        batchNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        rootNode.attachChild(batchNode);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setDirection(new Vector3f(-1f, -1f, -1f).normalizeLocal());
+        DirectionalLightShadowFilter shadowRenderer
+                = new DirectionalLightShadowFilter(assetManager, 1024, 2);
+        shadowRenderer.setEdgeFilteringMode(EdgeFilteringMode.PCF4);
+        shadowRenderer.setLambda(0.55f);
+        shadowRenderer.setLight(dl);
+        shadowRenderer.setShadowCompareMode(CompareMode.Hardware);
+        shadowRenderer.setShadowIntensity(0.6f);
+
+        int numSamples = settings.getSamples();
+        FilterPostProcessor fpp
+                = Heart.getFpp(viewPort, assetManager, numSamples);
+        fpp.addFilter(shadowRenderer);
+        viewPort.addProcessor(fpp);
+    }
+    // *************************************************************************
+    // private methods
+
+    private void addBrick(Vector3f ori) {
+        Geometry reBoxg = new Geometry("brick", brick);
+        reBoxg.setMaterial(mat);
+        reBoxg.setLocalTranslation(ori);
+        reBoxg.rotate(0f, MyMath.toRadians(angleDegrees), 0f);
+        reBoxg.addControl(new RigidBodyControl(1.5f));
+        reBoxg.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        reBoxg.getControl(RigidBodyControl.class).setFriction(1.6f);
+        reBoxg.getControl(RigidBodyControl.class).setLinearSleepingThreshold(0f);
+
+        batchNode.attachChild(reBoxg);
+        getPhysicsSpace().add(reBoxg);
+    }
+
+    private PhysicsSpace getPhysicsSpace() {
+        return bulletAppState.getPhysicsSpace();
+    }
+
+    private void initFloor() {
+        Box floorBox = new Box(10f, 0.1f, 5f);
+        floorBox.scaleTextureCoordinates(new Vector2f(3f, 6f));
+
+        Geometry floor = new Geometry("floor", floorBox);
+        floor.setMaterial(mat3);
+        floor.setShadowMode(RenderQueue.ShadowMode.Receive);
+        floor.addControl(new RigidBodyControl(PhysicsBody.massForStatic));
+        rootNode.attachChild(floor);
+        getPhysicsSpace().add(floor);
+    }
+
+    private void initMaterial() {
+        mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        TextureKey key = new TextureKey("Textures/Terrain/BrickWall/BrickWall.jpg");
+        key.setGenerateMips(true);
+        Texture tex = assetManager.loadTexture(key);
+        mat.setTexture("ColorMap", tex);
+
+        mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        TextureKey key2 = new TextureKey("Textures/Terrain/Rock/Rock.PNG");
+        key2.setGenerateMips(true);
+        Texture tex2 = assetManager.loadTexture(key2);
+        mat2.setTexture("ColorMap", tex2);
+
+        mat3 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        TextureKey key3 = new TextureKey("Textures/Terrain/Pond/Pond.jpg");
+        key3.setGenerateMips(true);
+        Texture tex3 = assetManager.loadTexture(key3);
+        tex3.setWrap(WrapMode.Repeat);
+        mat3.setTexture("ColorMap", tex3);
+    }
+
+    private void initTower() {
+        double tempX;
+        double tempY = 0.0;
+        double tempZ;
+        angleDegrees = 0f;
+        for (int i = 0; i < brickLayers; i++) {
+            // Increment rows
+            if (i != 0) {
+                tempY += brickHeight * 2;
+            } else {
+                tempY = brickHeight;
+            }
+            // Alternate brick seams
+            angleDegrees = 360f / bricksPerLayer * i / 2f;
+            for (int j = 0; j < bricksPerLayer; j++) {
+                tempZ = Math.cos(Math.toRadians(angleDegrees)) * radius;
+                tempX = Math.sin(Math.toRadians(angleDegrees)) * radius;
+                Vector3f vt = new Vector3f((float) tempX, (float) tempY, (float) tempZ);
+                // Add crenelation
+                if (i == brickLayers - 1) {
+                    if (j % 2 == 0) {
+                        addBrick(vt);
+                    }
+                } else { // Create main tower
+                    addBrick(vt);
+                }
+                angleDegrees += 360f / bricksPerLayer;
+            }
+        }
+    }
+
+    private void shoot() {
+        Geometry bulletg = new Geometry("bullet", bullet);
+        bulletg.setMaterial(mat2);
+        bulletg.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        bulletg.setLocalTranslation(cam.getLocation());
+
+        RigidBodyControl bulletNode
+                = new BombControl(assetManager, bulletCollisionShape, 1f);
+        bulletNode.setLinearVelocity(cam.getDirection().mult(25f));
+        bulletg.addControl(bulletNode);
+        rootNode.attachChild(bulletg);
+        getPhysicsSpace().add(bulletNode);
+
+        System.out.println("shoot");
+    }
+}
