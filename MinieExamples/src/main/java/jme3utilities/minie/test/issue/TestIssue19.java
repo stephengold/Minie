6b97ebb55dc2dd89e635e26581c964a3d887b052
Stephen Gold <sgold@sonic.net>
2021-11-26 14:57:51 -0800

    MinieExamples:  add the TestIssue19 application

---
+++ MinieExamples/src/main/java/jme3utilities/minie/test/issue/TestIssue19.java
@@ -0,0 +1,79 @@
+/*
+ Copyright (c) 2021, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test.issue;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+
+/**
+ * Test for Minie issue #19 (JRE crash with zero-thickness mesh shape).
+ *
+ * Based on "App.java" provided by Koteinik.
+ */
+public class TestIssue19 extends SimpleApplication {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the TestIssue19 application.
+     *
+     * @param ignored unused
+     */
+    public static void main(String[] ignored) {
+        TestIssue19 application = new TestIssue19();
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    @Override
+    public void simpleInitApp() {
+        BulletAppState bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+
+        Node playerNode = new Node();
+        rootNode.attachChild(playerNode);
+
+        BetterCharacterControl player
+                = new BetterCharacterControl(0.5f, 6f, 2f);
+        playerNode.addControl(player);
+        bulletAppState.getPhysicsSpace().add(player);
+
+        Box b = new Box(0f, 1f, 16f);
+        CollisionShape shape = new MeshCollisionShape(b);
+        PhysicsRigidBody phys
+                = new PhysicsRigidBody(shape, PhysicsBody.massForStatic);
+        bulletAppState.getPhysicsSpace().addCollisionObject(phys);
+    }
+}
