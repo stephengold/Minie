a91c784dd3a07a1400c88ed85a6326a55cc6058d
Stephen Gold <sgold@sonic.net>
2022-01-25 15:08:08 -0800

    buildscripts:  generate, sign, and deploy "module" metadata

--- bash/release.sh
+++ bash/release.sh
@@ -7,25 +7,25 @@ D=/home/sgold/Releases/Minie/$V
 mkdir $D
 
 ./gradlew clean release
-cp MinieLibrary/build/libs/*.{jar,pom} $D
+cp MinieLibrary/build/libs/*.{jar,module,pom} $D
 
 ./gradlew clean release -Pbare
-cp MinieLibrary/build/libs/*bare.{jar,pom} $D
+cp MinieLibrary/build/libs/*bare.{jar,module,pom} $D
 
 ./gradlew clean release -Pbig3
-cp MinieLibrary/build/libs/*big3.{jar,pom} $D
+cp MinieLibrary/build/libs/*big3.{jar,module,pom} $D
 
 ./gradlew clean release -Pbtdebug
-cp MinieLibrary/build/libs/*debug.{jar,pom} $D
+cp MinieLibrary/build/libs/*debug.{jar,module,pom} $D
 
 ./gradlew clean release -Pbtdebug -Pdp
-cp MinieLibrary/build/libs/*debugdp.{jar,pom} $D
+cp MinieLibrary/build/libs/*debugdp.{jar,module,pom} $D
 
 ./gradlew clean release -Pdp
-cp MinieLibrary/build/libs/*dp.{jar,pom} $D
+cp MinieLibrary/build/libs/*dp.{jar,module,pom} $D
 
 ./gradlew clean release -Pmt
-cp MinieLibrary/build/libs/*mt.{jar,pom} $D
+cp MinieLibrary/build/libs/*mt.{jar,module,pom} $D
 
 chmod 444 $D/*
 chmod 555 $D
