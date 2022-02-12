d5cda1c8d293e15df1ecc368df4155e89624f661
Stephen Gold <sgold@sonic.net>
2022-02-12 10:11:25 -0800

    MinieExamples:  add the JointElasticity example app

---
+++ MinieExamples/src/main/java/jme3utilities/minie/test/JointElasticity.java
@@ -0,0 +1,381 @@
+/*
+ Copyright (c) 2020-2022, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test;
+
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.joints.HingeJoint;
+import com.jme3.bullet.joints.JointEnd;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.font.Rectangle;
+import com.jme3.input.CameraInput;
+import com.jme3.input.KeyInput;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.system.AppSettings;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+import jme3utilities.Heart;
+import jme3utilities.MyCamera;
+import jme3utilities.Validate;
+import jme3utilities.minie.test.common.PhysicsDemo;
+import jme3utilities.ui.CameraOrbitAppState;
+import jme3utilities.ui.InputMode;
+
+/**
+ * Tune the simulation quality of a single-ended HingeJoint.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class JointElasticity extends PhysicsDemo {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * half the width of the door (in physics-space units)
+     */
+    final private static float doorHalfWidth = 2f;
+    /**
+     * mass of the door (in physics mass units)
+     */
+    final private static float doorMass = 1f;
+    /**
+     * half the thickness of the door (in physics-space units)
+     */
+    final private static float halfThickness = 0.3f;
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(JointElasticity.class.getName());
+    /**
+     * application name (for the title bar of the app's window)
+     */
+    final private static String applicationName
+            = JointElasticity.class.getSimpleName();
+    /**
+     * signal names for the CameraOrbitAppState
+     */
+    final public static String signalOrbitLeft = "cameraOrbitLeft";
+    final public static String signalOrbitRight = "cameraOrbitRight";
+    // *************************************************************************
+    // fields
+
+    /**
+     * AppState to manage the PhysicsSpace
+     */
+    private BulletAppState bulletAppState;
+    /**
+     * AppState to manage the status overlay
+     */
+    private JointElasticityStatus status;
+    /**
+     * dynamic ball
+     */
+    private PhysicsRigidBody ballBody;
+    /**
+     * dynamic door
+     */
+    private PhysicsRigidBody doorBody;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the JointElasticity application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        /*
+         * Mute the chatty loggers in certain packages.
+         */
+        Heart.setLoggingLevels(Level.WARNING);
+
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        try {
+            settings.load(applicationName);
+        } catch (BackingStoreException exception) {
+            logger.warning("Failed to load AppSettings.");
+        }
+        settings.setAudioRenderer(null);
+        settings.setGammaCorrection(true);
+        settings.setTitle(applicationName); // the window's title bar
+        settings.setVSync(true);
+
+        JointElasticity application = new JointElasticity();
+        application.setSettings(settings);
+        application.start();
+    }
+
+    /**
+     * Alter the mass ratio during a scenario.
+     *
+     * @param massRatio the mass of the ball as a multiple of the door's mass
+     * (&gt;0)
+     */
+    public void setMassRatio(float massRatio) {
+        Validate.positive(massRatio, "mass ratio");
+
+        float newMass = massRatio * doorMass;
+        ballBody.setMass(newMass);
+    }
+    // *************************************************************************
+    // PhysicsDemo methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void actionInitializeApplication() {
+        status = new JointElasticityStatus();
+        boolean success = stateManager.attach(status);
+        assert success;
+
+        configureCamera();
+        configureDumper();
+        configurePhysics();
+        restartScenario();
+
+        ColorRGBA bgColor = new ColorRGBA(0.04f, 0.04f, 0.04f, 1f);
+        viewPort.setBackgroundColor(bgColor);
+
+        setSpeed(0.2f);
+    }
+
+    /**
+     * Access the active BulletAppState.
+     *
+     * @return the pre-existing instance (not null)
+     */
+    @Override
+    protected BulletAppState getBulletAppState() {
+        assert bulletAppState != null;
+        return bulletAppState;
+    }
+
+    /**
+     * Determine the length of debug axis arrows (when they're visible).
+     *
+     * @return the desired length (in physics-space units, &ge;0)
+     */
+    @Override
+    protected float maxArrowLength() {
+        return 1f;
+    }
+
+    /**
+     * Add application-specific hotkey bindings and override existing ones.
+     */
+    @Override
+    public void moreDefaultBindings() {
+        InputMode dim = getDefaultInputMode();
+
+        dim.bind(asDumpScenes, KeyInput.KEY_P);
+        dim.bind(asDumpSpace, KeyInput.KEY_O);
+
+        dim.bind("next field", KeyInput.KEY_NUMPAD2);
+        dim.bind("next value", KeyInput.KEY_EQUALS, KeyInput.KEY_NUMPAD6);
+
+        dim.bindSignal(CameraInput.FLYCAM_LOWER, KeyInput.KEY_DOWN);
+        dim.bindSignal(CameraInput.FLYCAM_RISE, KeyInput.KEY_UP);
+        dim.bindSignal(signalOrbitLeft, KeyInput.KEY_LEFT);
+        dim.bindSignal(signalOrbitRight, KeyInput.KEY_RIGHT);
+
+        dim.bind("previous field", KeyInput.KEY_NUMPAD8);
+        dim.bind("previous value", KeyInput.KEY_MINUS, KeyInput.KEY_NUMPAD4);
+
+        dim.bind("restart", KeyInput.KEY_NUMPAD5, KeyInput.KEY_SPACE,
+                KeyInput.KEY_RETURN);
+
+        dim.bind(asToggleAabbs, KeyInput.KEY_APOSTROPHE);
+        dim.bind(asToggleHelp, KeyInput.KEY_H);
+        dim.bind(asTogglePause, KeyInput.KEY_PAUSE, KeyInput.KEY_PERIOD);
+        dim.bind(asTogglePcoAxes, KeyInput.KEY_SEMICOLON);
+        dim.bind(asToggleVArrows, KeyInput.KEY_K);
+        /*
+         * The help node can't be created until all hotkeys are bound.
+         */
+        addHelp();
+    }
+
+    /**
+     * Process an action that wasn't handled by the active input mode.
+     *
+     * @param actionString textual description of the action (not null)
+     * @param ongoing true if the action is ongoing, otherwise false
+     * @param tpf time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void onAction(String actionString, boolean ongoing, float tpf) {
+        if (ongoing) {
+            switch (actionString) {
+                case "next field":
+                    status.advanceSelectedField(+1);
+                    return;
+                case "next value":
+                    status.advanceValue(+1);
+                    return;
+                case "previous field":
+                    status.advanceSelectedField(-1);
+                    return;
+                case "previous value":
+                    status.advanceValue(-1);
+                    return;
+
+                case "restart":
+                    restartScenario();
+                    return;
+            }
+            String[] words = actionString.split(" ");
+            if (words.length == 2 && "load".equals(words[0])) {
+                return;
+            }
+        }
+        super.onAction(actionString, ongoing, tpf);
+    }
+
+    /**
+     * Callback invoked once per frame.
+     *
+     * @param tpf the time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Create a kinematic rigid body with a sphere shape and add it to the
+     * space.
+     */
+    private void addBall() {
+        float radius = 0.4f;
+        SphereCollisionShape shape = new SphereCollisionShape(radius);
+
+        float mass = status.massRatio() * doorMass;
+        ballBody = new PhysicsRigidBody(shape, mass);
+        ballBody.setLinearVelocity(new Vector3f(2f, 0f, -10f));
+        ballBody.setPhysicsLocation(new Vector3f(0.8f, 0f, 2f));
+        addCollisionObject(ballBody);
+    }
+
+    /**
+     * Create a dynamic body with a box shape and add it to the specified
+     * PhysicsSpace.
+     */
+    private void addDoor() {
+        float halfHeight = 4f;
+        BoxCollisionShape shape = new BoxCollisionShape(doorHalfWidth,
+                halfHeight, halfThickness);
+        doorBody = new PhysicsRigidBody(shape, doorMass);
+        addCollisionObject(doorBody);
+
+        // Disable sleep (deactivation).
+        doorBody.setEnableSleep(false);
+    }
+
+    /**
+     * Attach a Node to display hotkey help/hints.
+     */
+    private void addHelp() {
+        float margin = 10f; // in pixels
+        float width = 250f; // in pixels
+        float height = cam.getHeight() - (2f * margin);
+        float leftX = cam.getWidth() - (width + margin);
+        float topY = margin + height;
+        Rectangle rectangle = new Rectangle(leftX, topY, width, height);
+
+        attachHelpNode(rectangle);
+    }
+
+    /**
+     * Configure the camera during startup.
+     */
+    private void configureCamera() {
+        float near = 0.1f;
+        float far = 500f;
+        MyCamera.setNearFar(cam, near, far);
+
+        flyCam.setDragToRotate(true);
+        flyCam.setMoveSpeed(10f);
+        flyCam.setZoomSpeed(10f);
+
+        cam.setLocation(new Vector3f(0.0f, 8.8f, 6.2f));
+        cam.setRotation(new Quaternion(0.0f, 0.9f, -0.43589f, 0.0f));
+
+        CameraOrbitAppState orbitState = new CameraOrbitAppState(cam,
+                signalOrbitRight, signalOrbitLeft);
+        stateManager.attach(orbitState);
+    }
+
+    /**
+     * Create and configure a new PhysicsSpace.
+     */
+    private void configurePhysics() {
+        bulletAppState = new BulletAppState();
+        bulletAppState.setDebugEnabled(true);
+        stateManager.attach(bulletAppState);
+
+        PhysicsSpace physicsSpace = getPhysicsSpace();
+        int numIterations = status.numIterations();
+        physicsSpace.getSolverInfo().setNumIterations(numIterations);
+        float timestep = status.timeStep();
+        physicsSpace.setAccuracy(timestep);
+    }
+
+    /**
+     * Restart the scenario.
+     */
+    private void restartScenario() {
+        PhysicsSpace physicsSpace = getPhysicsSpace();
+        physicsSpace.destroy();
+
+        // Add a dynamic body for the door.
+        addDoor();
+
+        // Add a single-ended hinge joint to constrain the door's motion.
+        Vector3f pivotInDoor = new Vector3f(doorHalfWidth, 0f, 0f);
+        Vector3f pivotInWorld = new Vector3f(doorHalfWidth, 0f, 0f);
+        HingeJoint joint = new HingeJoint(doorBody, pivotInDoor, pivotInWorld,
+                Vector3f.UNIT_Y, Vector3f.UNIT_Y, JointEnd.B);
+        float lowAngle = 0;
+        float highAngle = 0;
+        joint.setLimit(lowAngle, highAngle); // disable rotation
+        addJoint(joint);
+
+        // Add a dynamic, yellow ball.
+        addBall();
+    }
+}
---
+++ MinieExamples/src/main/java/jme3utilities/minie/test/JointElasticityStatus.java
@@ -0,0 +1,304 @@
+/*
+ Copyright (c) 2020-2022, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.math.ColorRGBA;
+import java.util.Arrays;
+import java.util.logging.Logger;
+import jme3utilities.SimpleAppState;
+import jme3utilities.math.MyArray;
+import jme3utilities.math.MyMath;
+import jme3utilities.minie.test.common.PhysicsDemo;
+import jme3utilities.ui.AbstractDemo;
+
+/**
+ * AppState to display the status of the JontElasticity application in an
+ * overlay. The overlay consists of status lines, one of which is selected for
+ * editing. The overlay is located in the upper-left portion of the display.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class JointElasticityStatus extends SimpleAppState {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * list of constraint-solver iteration counts, in ascending order
+     */
+    final private static int[] iterationsValues
+            = {10, 250, 500, 1000, 2000, 4000};
+    /**
+     * list of mass ratios, in ascending order
+     */
+    final private static float[] ratioValues = {1f, 10f, 100f, 1000f};
+    /**
+     * list of physics timesteps, in ascending order
+     */
+    final private static float[] timestepValues
+            = {0.002f, 0.003f, 0.005f, 0.01f, 1f / 60, 0.04f};
+    /**
+     * index of the status line for the constraint-solver iteration count
+     */
+    final private static int iterationsStatusLine = 0;
+    /**
+     * index of the status line for the mass ratio
+     */
+    final private static int ratioStatusLine = 1;
+    /**
+     * index of the status line for physics timestep
+     */
+    final private static int timestepStatusLine = 2;
+    /**
+     * number of lines of text in the overlay
+     */
+    final private static int numStatusLines = 3;
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(JointElasticityStatus.class.getName());
+    // *************************************************************************
+    // fields
+
+    /**
+     * lines of text displayed in the upper-left corner of the GUI node ([0] is
+     * the top line)
+     */
+    final private BitmapText[] statusLines = new BitmapText[numStatusLines];
+    /**
+     * ball's mass as a multiple of the door
+     */
+    private float massRatio = 1f;
+    /**
+     * physics timestep
+     */
+    private float timestep = 1f / 60;
+    /**
+     * maximum number of solver iterations per physics timestep
+     */
+    private int numIterations = 10;
+    /**
+     * index of the line being edited (&ge;1)
+     */
+    private int selectedLine = timestepStatusLine;
+    /**
+     * reference to the application instance
+     */
+    private JointElasticity appInstance;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate an uninitialized enabled state.
+     */
+    JointElasticityStatus() {
+        super(true);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Advance the field selection by the specified amount.
+     *
+     * @param amount the number of fields to move downward
+     */
+    void advanceSelectedField(int amount) {
+        int firstField = 0;
+        int numFields = numStatusLines - firstField;
+
+        int selectedField = selectedLine - firstField;
+        int sum = selectedField + amount;
+        selectedField = MyMath.modulo(sum, numFields);
+        selectedLine = selectedField + firstField;
+    }
+
+    /**
+     * Advance the value of the selected field by the specified amount.
+     *
+     * @param amount
+     */
+    void advanceValue(int amount) {
+        switch (selectedLine) {
+            case iterationsStatusLine:
+                advanceIterations(amount);
+                break;
+
+            case ratioStatusLine:
+                advanceRatio(amount);
+                break;
+
+            case timestepStatusLine:
+                advanceTimestep(amount);
+                break;
+
+            default:
+                throw new IllegalStateException("line = " + selectedLine);
+        }
+    }
+
+    float massRatio() {
+        assert massRatio > 0f : massRatio;
+        return massRatio;
+    }
+
+    int numIterations() {
+        assert numIterations > 0 : numIterations;
+        return numIterations;
+    }
+
+    float timeStep() {
+        assert timestep > 0f : timestep;
+        return timestep;
+    }
+    // *************************************************************************
+    // ActionAppState methods
+
+    /**
+     * Clean up this AppState during the first update after it gets detached.
+     * Should be invoked only by a subclass or by the AppStateManager.
+     */
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        /*
+         * Remove the status lines from the guiNode.
+         */
+        for (int i = 0; i < numStatusLines; ++i) {
+            statusLines[i].removeFromParent();
+        }
+    }
+
+    /**
+     * Initialize this AppState on the first update after it gets attached.
+     *
+     * @param sm application's state manager (not null)
+     * @param app application which owns this state (not null)
+     */
+    @Override
+    public void initialize(AppStateManager sm, Application app) {
+        super.initialize(sm, app);
+
+        appInstance = (JointElasticity) app;
+        BitmapFont guiFont
+                = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        /*
+         * Add the status lines to the guiNode.
+         */
+        for (int lineIndex = 0; lineIndex < numStatusLines; ++lineIndex) {
+            statusLines[lineIndex] = new BitmapText(guiFont);
+            float y = cam.getHeight() - 20f * lineIndex;
+            statusLines[lineIndex].setLocalTranslation(0f, y, 0f);
+            guiNode.attachChild(statusLines[lineIndex]);
+        }
+
+        assert MyArray.isSorted(iterationsValues);
+        assert MyArray.isSorted(ratioValues);
+        assert MyArray.isSorted(timestepValues);
+    }
+
+    /**
+     * Callback to update this AppState prior to rendering. (Invoked once per
+     * frame while the state is attached and enabled.)
+     *
+     * @param tpf time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+
+        PhysicsSpace space = appInstance.getPhysicsSpace();
+        int index = 1 + Arrays.binarySearch(iterationsValues, numIterations);
+        String message = String.format("Solver iterations (#%d of %d):   %d",
+                index, iterationsValues.length, numIterations);
+        updateStatusLine(iterationsStatusLine, message);
+
+        index = 1 + Arrays.binarySearch(ratioValues, massRatio);
+        message = String.format("Mass ratio (#%d of %d):   %.0f : 1",
+                index, ratioValues.length, massRatio);
+        updateStatusLine(ratioStatusLine, message);
+
+        index = 1 + Arrays.binarySearch(timestepValues, timestep);
+        message = String.format("Timestep (#%d of %d):   %.4f second",
+                index, timestepValues.length, timestep);
+        updateStatusLine(timestepStatusLine, message);
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Advance the iteration-count selection by the specified amount.
+     *
+     * @param amount the number of values to advance (may be negative)
+     */
+    private void advanceIterations(int amount) {
+        numIterations = AbstractDemo.advanceInt(iterationsValues,
+                numIterations, amount);
+        PhysicsSpace physicsSpace = appInstance.getPhysicsSpace();
+        physicsSpace.getSolverInfo().setNumIterations(numIterations);
+    }
+
+    /**
+     * Advance the mass ratio by the specified amount.
+     *
+     * @param amount the number of values to advance (may be negative)
+     */
+    private void advanceRatio(int amount) {
+        massRatio = PhysicsDemo.advanceFloat(ratioValues, massRatio, amount);
+        appInstance.setMassRatio(massRatio);
+    }
+
+    /**
+     * Advance the timestep selection by the specified amount.
+     *
+     * @param amount the number of values to advance (may be negative)
+     */
+    private void advanceTimestep(int amount) {
+        timestep = PhysicsDemo.advanceFloat(timestepValues, timestep, amount);
+        PhysicsSpace physicsSpace = appInstance.getPhysicsSpace();
+        physicsSpace.setAccuracy(timestep);
+    }
+
+    /**
+     * Update the indexed status line.
+     */
+    private void updateStatusLine(int lineIndex, String text) {
+        BitmapText spatial = statusLines[lineIndex];
+
+        if (lineIndex == selectedLine) {
+            spatial.setColor(ColorRGBA.Yellow);
+            spatial.setText("-> " + text);
+        } else {
+            spatial.setColor(ColorRGBA.White);
+            spatial.setText(" " + text);
+        }
+    }
+}
