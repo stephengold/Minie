011d2f199b754f03dcc78913771ec423fe6dfa79
Stephen Gold <sgold@sonic.net>
2022-02-14 12:11:26 -0800

    bash:  move projects to the NetBeans 12.6  project folder

--- bash/meld.sh
+++ bash/meld.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-S1=/home/sgold/Git/Libbulletjme/src/main/java
-D1=/home/sgold/Git/Minie/MinieLibrary/src/main/java
+GitDir=~/NetBeansProjects
 
-S2=/home/sgold/Git/jmonkeyengine/jme3-examples/src/main/java
-D2=/home/sgold/Git/Minie/Jme3Examples/src/main/java
+S1=$GitDir/Libbulletjme/src/main/java
+D1=$GitDir/Minie/MinieLibrary/src/main/java
+
+S2=$GitDir/jmonkeyengine/jme3-examples/src/main/java
+D2=$GitDir/Minie/Jme3Examples/src/main/java
 
 /usr/bin/meld --diff $S1 $D1 --diff $S2 $D2
