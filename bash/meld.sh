1d0788bdaf2ae510f9ea5e0ff2c73e0bd8bd9e08
Stephen Gold <sgold@sonic.net>
2022-04-26 10:15:24 -0700

    meld.sh:  also compare TutorialApps with LbjExamples

--- bash/meld.sh
+++ bash/meld.sh
@@ -10,4 +10,7 @@ D1=$GitDir/Minie/MinieLibrary/src/main/java
 S2=$GitDir/jmonkeyengine/jme3-examples/src/main/java
 D2=$GitDir/Minie/Jme3Examples/src/main/java
 
-/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2
+S3=$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps
+D3=$GitDir/Minie/TutorialApps/src/main/java/jme3utilities/tutorial
+
+/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3
