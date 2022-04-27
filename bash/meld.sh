fef3a2f2dc58a3d4b0891d8be736eabe80a7af56
Stephen Gold <sgold@sonic.net>
2022-04-26 22:13:01 -0700

    meld.sh:  also compare website with LbjExamples

--- bash/meld.sh
+++ bash/meld.sh
@@ -13,4 +13,10 @@ D2=$GitDir/Minie/Jme3Examples/src/main/java
 S3=$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps
 D3=$GitDir/Minie/TutorialApps/src/main/java/jme3utilities/tutorial
 
-/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3
+S4=$GitDir/LbjExamples/docs/en/modules/ROOT
+D4=$GitDir/Minie/MinieLibrary/src/site/antora/tutorials/modules/mini-library-tutorials
+
+S5=$GitDir/LbjExamples/docs/en/modules/ROOT
+D5=$GitDir/Minie/src/site/antora/minie-project/modules/ROOT
+
+/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3 --diff $S4 $D4 --diff $S5 $D5
