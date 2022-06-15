a915ac901bf84afbf8c45702a2ae4e1ab3c8d3e6
Stephen Gold <sgold@sonic.net>
2022-06-15 10:52:43 -0700

    TutorialApps:  add HelloGhost

---
+++ TutorialApps/src/main/java/jme3utilities/tutorial/HelloGhost.java
@@ -0,0 +1,398 @@
+/*
+ Copyright (c) 2020-2022, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.TextureKey;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.PlaneCollisionShape;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.debug.DebugInitListener;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsCharacter;
+import com.jme3.bullet.objects.PhysicsGhostObject;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.bullet.util.PlaneDmiListener;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.InputListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Plane;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Limits;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.shadow.EdgeFilteringMode;
+import com.jme3.system.AppSettings;
+import com.jme3.texture.Texture;
+import jme3utilities.minie.FilterAll;
+
+/**
+ * A simple example of a ghost object.
+ * <p>
+ * Press the arrow keys to walk. Press the space bar to jump.
+ * <p>
+ * Builds upon HelloWalk.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HelloGhost
+        extends SimpleApplication
+        implements PhysicsTickListener {
+    // *************************************************************************
+    // fields
+
+    /**
+     * text displayed in the upper-left corner of the GUI node
+     */
+    private BitmapText statusText;
+    /**
+     * true when the spacebar is pressed, otherwise false
+     */
+    private volatile boolean jumpRequested;
+    /**
+     * true when the DOWN key is pressed, otherwise false
+     */
+    private volatile boolean walkBackward;
+    /**
+     * true when the UP key is pressed, otherwise false
+     */
+    private volatile boolean walkForward;
+    /**
+     * true when the LEFT key is pressed, otherwise false
+     */
+    private volatile boolean walkLeft;
+    /**
+     * true when the RIGHT key is pressed, otherwise false
+     */
+    private volatile boolean walkRight;
+
+    private BulletAppState bulletAppState;
+    private PhysicsCharacter character;
+    private PhysicsGhostObject ghost;
+    /**
+     * PhysicsSpace for simulation
+     */
+    private PhysicsSpace physicsSpace;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the HelloGhost application.
+     *
+     * @param arguments array of command-line arguments (not null)
+     */
+    public static void main(String[] arguments) {
+        HelloGhost application = new HelloGhost();
+
+        // Enable gamma correction for accurate lighting.
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        settings.setGammaCorrection(true);
+        application.setSettings(settings);
+
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        configureCamera();
+        configureInput();
+        this.physicsSpace = configurePhysics();
+
+        // Add the status text to the GUI.
+        statusText = new BitmapText(guiFont);
+        statusText.setLocalTranslation(205f, 25f, 0f);
+        guiNode.attachChild(statusText);
+
+        // Create a ghost using a sphere shape and add it to the space.
+        float sphereRadius = 10f;
+        SphereCollisionShape sphereShape
+                = new SphereCollisionShape(sphereRadius);
+        this.ghost = new PhysicsGhostObject(sphereShape);
+        ghost.setPhysicsLocation(new Vector3f(15f, 0f, -13f));
+        physicsSpace.addCollisionObject(ghost);
+
+        // Create a character with a capsule shape and add it to the space.
+        float capsuleRadius = 3f;
+        float capsuleHeight = 4f;
+        CapsuleCollisionShape shape
+                = new CapsuleCollisionShape(capsuleRadius, capsuleHeight);
+        float stepHeight = 0.01f;
+        character = new PhysicsCharacter(shape, stepHeight);
+        character.setGravity(4f);
+        physicsSpace.addCollisionObject(character);
+
+        // Add a plane to represent the ground.
+        float y = -2f;
+        addPlane(y);
+    }
+
+    /**
+     * Callback invoked once per frame.
+     *
+     * @param tpf the time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+        updateStatusText();
+    }
+    // *************************************************************************
+    // PhysicsTickListener methods
+
+    /**
+     * Callback from Bullet, invoked just before each simulation step.
+     *
+     * @param space the space that's about to be stepped (not null)
+     * @param timeStep the time per simulation step (in seconds, &ge;0)
+     */
+    @Override
+    public void prePhysicsTick(PhysicsSpace space, float timeStep) {
+        // Clear any motion from the previous simulation step.
+        character.setWalkDirection(Vector3f.ZERO);
+        /*
+         * If the character is touching the ground,
+         * cause it respond to keyboard input.
+         */
+        if (character.onGround()) {
+            if (jumpRequested) {
+                character.jump();
+
+            } else {
+                // Walk as directed.
+                Vector3f offset = cam.getDirection();
+                float backward = walkBackward ? 1f : 0f;
+                float forward = walkForward ? 1f : 0f;
+                offset.multLocal(forward - backward);
+                float right = walkRight ? 1f : 0f;
+                float left = walkLeft ? 1f : 0f;
+                offset = cam.getLeft().mult(left - right).add(offset);
+
+                offset.y = 0f;
+                if (offset.lengthSquared() != 0f) {
+                    float scale = 7f * timeStep / offset.length();
+                    offset.multLocal(scale);
+                }
+                character.setWalkDirection(offset);
+            }
+        }
+    }
+
+    /**
+     * Callback from Bullet, invoked just after each simulation step.
+     *
+     * @param space the space that was just stepped (not null)
+     * @param timeStep the time per simulation step (in seconds, &ge;0)
+     */
+    @Override
+    public void physicsTick(PhysicsSpace space, float timeStep) {
+        // do nothing
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add lighting and shadows to the specified scene and set the background
+     * color.
+     */
+    private void addLighting(Spatial scene) {
+        ColorRGBA ambientColor = new ColorRGBA(0.03f, 0.03f, 0.03f, 1f);
+        AmbientLight ambient = new AmbientLight(ambientColor);
+        scene.addLight(ambient);
+        ambient.setName("ambient");
+
+        ColorRGBA directColor = new ColorRGBA(0.3f, 0.3f, 0.3f, 1f);
+        Vector3f direction = new Vector3f(-7f, -3f, -5f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(direction, directColor);
+        scene.addLight(sun);
+        sun.setName("sun");
+
+        // Render shadows based on the directional light.
+        viewPort.clearProcessors();
+        int shadowMapSize = 2_048; // in pixels
+        int numSplits = 3;
+        DirectionalLightShadowRenderer dlsr
+                = new DirectionalLightShadowRenderer(assetManager,
+                        shadowMapSize, numSplits);
+        dlsr.setEdgeFilteringMode(EdgeFilteringMode.PCFPOISSON);
+        dlsr.setEdgesThickness(5);
+        dlsr.setLight(sun);
+        dlsr.setShadowIntensity(0.4f);
+        viewPort.addProcessor(dlsr);
+
+        // Set the background color to light blue.
+        ColorRGBA skyColor = new ColorRGBA(0.1f, 0.2f, 0.4f, 1f);
+        viewPort.setBackgroundColor(skyColor);
+    }
+
+    /**
+     * Add a horizontal plane body to the space.
+     *
+     * @param y (the desired elevation, in physics-space coordinates)
+     */
+    private void addPlane(float y) {
+        Plane plane = new Plane(Vector3f.UNIT_Y, y);
+        PlaneCollisionShape shape = new PlaneCollisionShape(plane);
+        PhysicsRigidBody body
+                = new PhysicsRigidBody(shape, PhysicsBody.massForStatic);
+
+        // Load a repeating tile texture.
+        String assetPath = "Textures/greenTile.png";
+        boolean flipY = false;
+        TextureKey key = new TextureKey(assetPath, flipY);
+        boolean generateMips = true;
+        key.setGenerateMips(generateMips);
+        Texture texture = assetManager.loadTexture(key);
+        texture.setMinFilter(Texture.MinFilter.Trilinear);
+        texture.setWrap(Texture.WrapMode.Repeat);
+
+        // Enable anisotropic filtering, to reduce blurring.
+        Integer maxDegree = renderer.getLimits().get(Limits.TextureAnisotropy);
+        int degree = (maxDegree == null) ? 1 : Math.min(8, maxDegree);
+        texture.setAnisotropicFilter(degree);
+
+        // Apply a tiled, unshaded debug material to the body.
+        Material material = new Material(assetManager, Materials.UNSHADED);
+        material.setTexture("ColorMap", texture);
+        body.setDebugMaterial(material);
+
+        // Generate texture coordinates during debug-mesh initialization.
+        float tileSize = 1f;
+        PlaneDmiListener planeDmiListener = new PlaneDmiListener(tileSize);
+        body.setDebugMeshInitListener(planeDmiListener);
+
+        FilterAll filter = new FilterAll(true);
+        filter.addException(body);
+        bulletAppState.setDebugBoundingBoxFilter(filter);
+
+        physicsSpace.addCollisionObject(body);
+    }
+
+    /**
+     * Configure the Camera during startup.
+     */
+    private void configureCamera() {
+        flyCam.setEnabled(false);
+
+        cam.setLocation(new Vector3f(26f, 23f, 35f));
+        cam.setRotation(new Quaternion(-0.04f, 0.95897f, -0.20975f, -0.18631f));
+    }
+
+    /**
+     * Configure keyboard input during startup.
+     */
+    private void configureInput() {
+        inputManager.addMapping("backward", new KeyTrigger(KeyInput.KEY_DOWN));
+        inputManager.addMapping("forward", new KeyTrigger(KeyInput.KEY_UP));
+        inputManager.addMapping("jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("left", new KeyTrigger(KeyInput.KEY_LEFT));
+        inputManager.addMapping("right", new KeyTrigger(KeyInput.KEY_RIGHT));
+
+        InputListener input = new ActionListener() {
+            @Override
+            public void onAction(String action, boolean isPressed, float tpf) {
+                switch (action) {
+                    case "backward":
+                        walkBackward = isPressed;
+                        return;
+                    case "forward":
+                        walkForward = isPressed;
+                        return;
+                    case "jump":
+                        jumpRequested = isPressed;
+                        return;
+                    case "left":
+                        walkLeft = isPressed;
+                        return;
+                    case "right":
+                        walkRight = isPressed;
+                        return;
+
+                    default:
+                        System.out.println("Unknown action: " + action);
+                }
+            }
+        };
+        inputManager.addListener(input,
+                "backward", "forward", "jump", "left", "right");
+    }
+
+    /**
+     * Configure physics during startup.
+     */
+    private PhysicsSpace configurePhysics() {
+        this.bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+
+        // Enable debug visualization to reveal what occurs in physics space.
+        bulletAppState.setDebugEnabled(true);
+
+        // Add lighting and shadows to the debug scene.
+        bulletAppState.setDebugInitListener(new DebugInitListener() {
+            @Override
+            public void bulletDebugInit(Node physicsDebugRootNode) {
+                addLighting(physicsDebugRootNode);
+            }
+        });
+        bulletAppState.setDebugShadowMode(
+                RenderQueue.ShadowMode.CastAndReceive);
+
+        PhysicsSpace result = bulletAppState.getPhysicsSpace();
+
+        // Activate the PhysicsTickListener interface.
+        result.addTickListener(this);
+
+        return result;
+    }
+
+    /**
+     * Update the status text in the GUI.
+     */
+    private void updateStatusText() {
+        int overlappingCount = ghost.getOverlappingCount();
+        String message = "overlapping count = " + overlappingCount;
+        statusText.setText(message);
+    }
+}
