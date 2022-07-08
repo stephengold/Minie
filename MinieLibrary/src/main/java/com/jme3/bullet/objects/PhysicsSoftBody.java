1d7ff0ae22e84a172988461f5495459288a6655e
Stephen Gold <sgold@sonic.net>
2022-07-07 18:32:43 -0700

    PhysicsSoftBody:  trivial refactoring for clarity

--- MinieLibrary/src/main/java/com/jme3/bullet/objects/PhysicsSoftBody.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/objects/PhysicsSoftBody.java
@@ -137,7 +137,7 @@ public class PhysicsSoftBody extends PhysicsBody {
      * space.
      */
     public PhysicsSoftBody() {
-        worldInfo = new SoftBodyWorldInfo();
+        this.worldInfo = new SoftBodyWorldInfo();
         long infoId = worldInfo.nativeId();
         long bodyId = createEmpty(infoId);
         super.setNativeId(bodyId);
@@ -145,7 +145,7 @@ public class PhysicsSoftBody extends PhysicsBody {
                 getInternalType(bodyId);
         logger2.log(Level.FINE, "Created {0}.", this);
 
-        config = new SoftBodyConfig(this);
+        this.config = new SoftBodyConfig(this);
         super.initUserPointer();
 
         float defaultMargin = CollisionShape.getDefaultMargin();
@@ -830,7 +830,7 @@ public class PhysicsSoftBody extends PhysicsBody {
      */
     public SoftBodyMaterial getSoftMaterial() {
         if (material == null) {
-            material = new SoftBodyMaterial(this);
+            this.material = new SoftBodyMaterial(this);
         }
 
         return material;
@@ -1352,8 +1352,8 @@ public class PhysicsSoftBody extends PhysicsBody {
             unassignNativeObject();
         }
 
-        material = null;
-        config = null;
+        this.material = null;
+        this.config = null;
     }
 
     /**
@@ -1379,7 +1379,7 @@ public class PhysicsSoftBody extends PhysicsBody {
                 getInternalType(objectId);
         logger2.log(Level.FINE, "Created {0}.", this);
 
-        config = new SoftBodyConfig(this);
+        this.config = new SoftBodyConfig(this);
         initUserPointer();
 
         assert !isInWorld();
@@ -1424,7 +1424,7 @@ public class PhysicsSoftBody extends PhysicsBody {
      */
     @Override
     public void cloneFields(Cloner cloner, Object original) {
-        worldInfo = cloner.clone(worldInfo);
+        this.worldInfo = cloner.clone(worldInfo);
         newEmptySoftBody(); // needs worldInfo!
 
         super.cloneFields(cloner, original);
@@ -1432,7 +1432,7 @@ public class PhysicsSoftBody extends PhysicsBody {
         cloneIgnoreList(cloner, old);
         copyPcoProperties(old);
         config.copyAll(old.config);
-        material = cloner.clone(old.material);
+        this.material = cloner.clone(old.material);
 
         FloatBuffer floats = old.copyLocations(null);
         appendNodes(floats);
@@ -1594,18 +1594,20 @@ public class PhysicsSoftBody extends PhysicsBody {
     public void read(JmeImporter importer) throws IOException {
         InputCapsule capsule = importer.getCapsule(this);
 
-        worldInfo = (SoftBodyWorldInfo) capsule.readSavable(tagWorldInfo, null);
+        this.worldInfo
+                = (SoftBodyWorldInfo) capsule.readSavable(tagWorldInfo, null);
         newEmptySoftBody(); // needs worldInfo!
 
         super.read(importer);
         readPcoProperties(capsule);
-        isWorldInfoProtected
+        this.isWorldInfoProtected
                 = capsule.readBoolean(tagIsWorldInfoProtected, false);
 
-        config = (SoftBodyConfig) capsule.readSavable(tagConfig, null);
+        this.config = (SoftBodyConfig) capsule.readSavable(tagConfig, null);
         assert config != null;
 
-        material = (SoftBodyMaterial) capsule.readSavable(tagMaterial, null);
+        this.material
+                = (SoftBodyMaterial) capsule.readSavable(tagMaterial, null);
 
         float[] fArray = capsule.readFloatArray(tagNodeLocations, new float[0]);
         appendNodes(BufferUtils.createFloatBuffer(fArray));
