6671279f672fbabaa77f9d553786bac2d079447f
Stephen Gold <sgold@sonic.net>
2022-08-02 20:36:21 -0700

    PhysicsCollisionObject:  de-privatize setIgnoreCollisionCheck()

--- MinieLibrary/src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
@@ -1372,6 +1372,16 @@ abstract public class PhysicsCollisionObject
             int desiredFlags);
 
     /**
+     * Alter the ignore list for collisions.
+     *
+     * @param object1Id the ID of the first btCollisionObject (not zero)
+     * @param object2Id the ID of the 2nd btCollisionObject (not zero)
+     * @param setting true to ignore, false to stop ignoring
+     */
+    native protected static void setIgnoreCollisionCheck(
+            long object1Id, long object2Id, boolean setting);
+
+    /**
      * Directly alter this object's location and basis.
      *
      * @param centerLocation the desired location for this object's center (in
@@ -1650,9 +1660,6 @@ abstract public class PhysicsCollisionObject
     native private static void setDeactivationTime(long objectId, float time);
 
     native private static void setFriction(long objectId, float friction);
-
-    native private static void setIgnoreCollisionCheck(long object1Id,
-            long object2Id, boolean setting);
 
     native private static void setLocationAndBasis(long objectId,
             Vector3f location, Matrix3f basis);
