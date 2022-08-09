8c9b63ebd7bcc4d8bf8949aebd6f373a07e84241
Stephen Gold <sgold@sonic.net>
2022-08-09 09:12:46 -0700

    library:  add the FillMode enum

---
+++ MinieLibrary/src/main/java/com/jme3/bullet/FillMode.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.bullet;
+
+/**
+ * Enumerate algorithms to generate a solid object from voxels (native name:
+ * FillMode).
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public enum FillMode {
+    // *************************************************************************
+    // values
+
+    /**
+     * use a flood fill to distinguish inside from outside (native name:
+     * FLOOD_FILL)
+     */
+    FloodFill,
+    /**
+     * create a hollow skin (native name: SURFACE_ONLY)
+     */
+    SurfaceOnly,
+    /**
+     * use raycasting to distinguish inside from outside (native name:
+     * RAYCAST_FILL)
+     */
+    RaycastFill;
+}
