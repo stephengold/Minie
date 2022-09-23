b06bfb7dc4fe5c7ac05bd2b5f781d1f75e8ccd1e
Stephen Gold <sgold@sonic.net>
2022-09-23 12:02:17 -0700

    MeshCollisionShape:  add a constructor based on a CompoundMesh

--- MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/MeshCollisionShape.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/MeshCollisionShape.java
@@ -119,6 +119,21 @@ public class MeshCollisionShape extends CollisionShape {
     }
 
     /**
+     * Instantiate a shape based on the specified CompoundMesh.
+     *
+     * @param useCompression true to use quantized AABB compression
+     * @param mesh the mesh on which to base the shape (not null, must contain
+     * at least one triangle, unaffected)
+     */
+    public MeshCollisionShape(boolean useCompression, CompoundMesh mesh) {
+        Validate.require(mesh.countTriangles() > 0, "at least one triangle");
+
+        this.nativeMesh = new CompoundMesh(mesh);
+        this.useCompression = useCompression;
+        createShape();
+    }
+
+    /**
      * Instantiate a shape from the specified native mesh(es).
      *
      * @param useCompression true to use quantized AABB compression
