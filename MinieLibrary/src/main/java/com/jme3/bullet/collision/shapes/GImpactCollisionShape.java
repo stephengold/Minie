ef535fb14ba89728acd2d7deffd3c5288d55990b
Stephen Gold <sgold@sonic.net>
2022-09-23 13:25:20 -0700

    GImpactCollisionShape:  add the countMeshTriangles() method

--- MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/GImpactCollisionShape.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/collision/shapes/GImpactCollisionShape.java
@@ -120,6 +120,16 @@ public class GImpactCollisionShape extends CollisionShape {
     // new methods exposed
 
     /**
+     * Count how many triangles are in the mesh.
+     *
+     * @return the count (&ge;0)
+     */
+    public int countMeshTriangles() {
+        int result = nativeMesh.countTriangles();
+        return result;
+    }
+
+    /**
      * Count how many vertices are in the mesh.
      *
      * @return the count (&ge;0)
@@ -192,7 +202,7 @@ public class GImpactCollisionShape extends CollisionShape {
      *
      * @param cloner the Cloner that's cloning this shape (not null)
      * @param original the instance from which this shape was shallow-cloned
-     * (not null)
+     * (not null, unaffected)
      */
     @Override
     public void cloneFields(Cloner cloner, Object original) {
