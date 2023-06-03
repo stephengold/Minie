8624173bba37cc957da38d324b07dafbc19931ec
Stephen Gold <sgold@sonic.net>
2023-06-03 13:34:53 -0700

    update the native libraries to Libbulletjme v18.3.0

--- MinieLibrary/src/main/java/com/jme3/bullet/util/NativeLibrary.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/util/NativeLibrary.java
@@ -52,7 +52,7 @@ final public class NativeLibrary {
     /**
      * expected version string of the native library
      */
-    final public static String expectedVersion = "18.2.1";
+    final public static String expectedVersion = "18.3.0";
     // *************************************************************************
     // constructors
 
