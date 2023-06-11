cac6305036eee18eb4326621672b91e15992a254
Stephen Gold <sgold@sonic.net>
2023-06-11 16:26:46 -0700

    TestSetScale:  test a MultiSphere

--- MinieLibrary/src/test/java/jme3utilities/minie/test/TestSetScale.java
+++ MinieLibrary/src/test/java/jme3utilities/minie/test/TestSetScale.java
@@ -44,6 +44,7 @@ import com.jme3.bullet.collision.shapes.GImpactCollisionShape;
 import com.jme3.bullet.collision.shapes.HeightfieldCollisionShape;
 import com.jme3.bullet.collision.shapes.HullCollisionShape;
 import com.jme3.bullet.collision.shapes.MeshCollisionShape;
+import com.jme3.bullet.collision.shapes.MultiSphere;
 import com.jme3.bullet.collision.shapes.PlaneCollisionShape;
 import com.jme3.bullet.collision.shapes.SimplexCollisionShape;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
@@ -60,6 +61,8 @@ import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Image;
 import com.jme3.texture.Texture;
 import com.jme3.texture.plugins.AWTLoader;
+import java.util.ArrayList;
+import java.util.List;
 import jme3utilities.MyAsset;
 import org.junit.Test;
 
@@ -216,6 +219,24 @@ public class TestSetScale {
         assert mcs.getScale(null).equals(non);
         mcs.setScale(non2);
         assert mcs.getScale(null).equals(non2);
+
+        // MultiSphere
+        List<Float> radii = new ArrayList<>(3);
+        List<Vector3f> centers = new ArrayList<>(3);
+        radii.add(0.5f);
+        centers.add(new Vector3f(0f, 1f, 0f));
+        radii.add(1f);
+        centers.add(new Vector3f(0f, 0f, 0f));
+        radii.add(0.5f);
+        centers.add(new Vector3f(1f, 0f, 0f));
+        CollisionShape multiSphere = new MultiSphere(centers, radii);
+        assert multiSphere.getScale(null).equals(ident);
+        multiSphere.setScale(uni);
+        assert multiSphere.getScale(null).equals(uni);
+        multiSphere.setScale(non);
+        assert multiSphere.getScale(null).equals(non);
+        multiSphere.setScale(non2);
+        assert multiSphere.getScale(null).equals(non2);
 
         // PlaneCollisionShape
         Plane plane = new Plane(new Vector3f(0f, 1f, 0f), 0f);
