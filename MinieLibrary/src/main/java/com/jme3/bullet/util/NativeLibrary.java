1caa0326f794e80220b0a3104b890bdd0c844735
Stephen Gold <sgold@sonic.net>
2023-12-01 17:27:18 -0800

    NativeLibrary:  update the expected version string to 18.6.0

--- MinieLibrary/src/main/java/com/jme3/bullet/util/NativeLibrary.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/util/NativeLibrary.java
@@ -52,7 +52,7 @@ final public class NativeLibrary {
     /**
      * expected version string of the native library
      */
-    final public static String expectedVersion = "18.5.2";
+    final public static String expectedVersion = "18.6.0";
     // *************************************************************************
     // constructors
 
