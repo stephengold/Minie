62a7fc0074e9c43e824d1341ed80dfedd9cd0ca4
Stephen Gold <sgold@sonic.net>
2024-01-16 21:50:16 -0800

    PhysicsCollisionObject: privatize setIgnoreCollisionCheck (API change)

--- MinieLibrary/src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/collision/PhysicsCollisionObject.java
@@ -1579,16 +1579,6 @@ abstract public class PhysicsCollisionObject
             setCollisionFlags(long objectId, int desiredFlags);
 
     /**
-     * Alter the ignore list for collisions. TODO privatize
-     *
-     * @param object1Id the ID of the first btCollisionObject (not zero)
-     * @param object2Id the ID of the 2nd btCollisionObject (not zero)
-     * @param setting true to ignore, false to stop ignoring
-     */
-    native protected static void setIgnoreCollisionCheck(
-            long object1Id, long object2Id, boolean setting);
-
-    /**
      * Directly alter this object's location and basis.
      *
      * @param centerLocation the desired location for this object's center (in
@@ -1902,6 +1892,9 @@ abstract public class PhysicsCollisionObject
     native private static void setDeactivationTime(long objectId, float time);
 
     native private static void setFriction(long objectId, float friction);
+
+    native private static void setIgnoreCollisionCheck(
+            long object1Id, long object2Id, boolean setting);
 
     native private static void setLocationAndBasis(
             long objectId, Vector3f location, Matrix3f basis);
