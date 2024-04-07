1c0df1e97d9df0627d53cbcb08a2f9d5263495dd
stephengold <sgold@sonic.net>
2024-04-06 19:16:41 -0700

    examples:  add the TestIssue40 app

---
+++ MinieExamples/src/main/java/jme3utilities/minie/test/issue/TestIssue40.java
@@ -0,0 +1,125 @@
+/*
+ Copyright (c) 2024 Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.minie.test.issue;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.collision.shapes.HeightfieldCollisionShape;
+import com.jme3.bullet.collision.shapes.HullCollisionShape;
+import com.jme3.bullet.objects.PhysicsBody;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.material.Material;
+import com.jme3.material.Materials;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.shape.Sphere;
+import java.util.logging.Logger;
+import jme3utilities.minie.PhysicsDumper;
+
+/**
+ * Test for Minie issue #40 (certain shapes fall through a flat terrain with
+ * contact filtering).
+ * <p>
+ * Collision objects are rendered entirely by debug visualization.
+ * <p>
+ * If successful, the ball will land on the square heightmap. If unsuccessful,
+ * it will pass through and keep going.
+ * <p>
+ * Based on "TerrainTest.java" provided by ndebruyn.
+ */
+final public class TestIssue40 extends SimpleApplication {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger
+            = Logger.getLogger(TestIssue40.class.getName());
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate the TestIssue40 application.
+     * <p>
+     * This no-arg constructor was made explicit to avoid javadoc warnings from
+     * JDK 18+.
+     */
+    public TestIssue40() {
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the TestIssue40 application.
+     *
+     * @param arguments unused
+     */
+    public static void main(String[] arguments) {
+        TestIssue40 application = new TestIssue40();
+        application.start();
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize the application.
+     */
+    @Override
+    public void simpleInitApp() {
+        flyCam.setEnabled(false);
+        cam.setLocation(new Vector3f(-10f, 5f, 10f));
+        cam.setRotation(new Quaternion(0.064f, 0.9106f, -0.156f, 0.377f));
+
+        BulletAppState bulletAppState = new BulletAppState();
+        bulletAppState.setDebugEnabled(true);
+        stateManager.attach(bulletAppState);
+        PhysicsSpace physicsSpace = bulletAppState.getPhysicsSpace();
+
+        Sphere sphere = new Sphere(5, 7, 1f);
+        HullCollisionShape hullShape = new HullCollisionShape(sphere);
+        PhysicsRigidBody ballBody = new PhysicsRigidBody(hullShape);
+        ballBody.setPhysicsLocation(new Vector3f(0f, 5f, 0f));
+        physicsSpace.add(ballBody);
+
+        CollisionShape heightShape = new HeightfieldCollisionShape(
+                new float[9], new Vector3f(2f, 1f, 2f));
+        PhysicsRigidBody terrainBody
+                = new PhysicsRigidBody(heightShape, PhysicsBody.massForStatic);
+        //heightShape.setContactFilterEnabled(false); // default=true
+        physicsSpace.add(terrainBody);
+
+        Material solidGray = new Material(assetManager, Materials.UNSHADED);
+        solidGray.setColor("Color", ColorRGBA.DarkGray);
+        terrainBody.setDebugMaterial(solidGray);
+
+        new PhysicsDumper().dump(bulletAppState);
+    }
+}
