1cc19d6df571ec18f5efc814f86ee4e7fe06beac
stephengold <sgold@sonic.net>
2024-04-22 11:24:13 -0700

    PhysicsSpace:  add another 5-arg constructor

--- MinieLibrary/src/main/java/com/jme3/bullet/PhysicsSpace.java
+++ MinieLibrary/src/main/java/com/jme3/bullet/PhysicsSpace.java
@@ -251,6 +251,26 @@ public class PhysicsSpace
     }
 
     /**
+     * Instantiate a PhysicsSpace with the specified number of
+     * sequential-impulse solvers. Must be invoked on the designated physics
+     * thread.
+     *
+     * @param worldMin the desired minimum coordinate values (not null,
+     * unaffected, default=(-10k,-10k,-10k))
+     * @param worldMax the desired maximum coordinate values (not null,
+     * unaffected, default=(10k,10k,10k))
+     * @param broadphaseType which broadphase accelerator to use (not null)
+     * @param numSolvers the desired number of solvers in the thread-safe pool
+     * (&ge;1, &le;64, default=numThreads)
+     * @param configuration the desired configuration (not null)
+     */
+    public PhysicsSpace(
+            Vector3f worldMin, Vector3f worldMax, BroadphaseType broadphaseType,
+            int numSolvers, CollisionConfiguration configuration) {
+        super(worldMin, worldMax, broadphaseType, numSolvers, configuration);
+    }
+
+    /**
      * Instantiate a PhysicsSpace with the specified contact-and-constraint
      * solver. Must be invoked on the designated physics thread.
      *
