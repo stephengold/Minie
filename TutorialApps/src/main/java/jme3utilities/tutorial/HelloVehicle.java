b5ef92ca917d66c73b47b62027548dd471e30d45
stephengold <sgold@sonic.net>
2025-10-05 11:41:39 -0700

    HelloVehicle:  rename 2 local variables

--- TutorialApps/src/main/java/jme3utilities/tutorial/HelloVehicle.java
+++ TutorialApps/src/main/java/jme3utilities/tutorial/HelloVehicle.java
@@ -120,20 +120,20 @@ public class HelloVehicle extends SimpleApplication {
         // Add 4 wheels, 2 in the front (for steering) and 2 in the rear.
         boolean front = true;
         boolean rear = false;
-        float frontAxisZ = 0.7f * noseZ; // offset from chassis center
-        float rearAxisZ = 0.8f * tailZ; // offset from chassis center
+        float frontAxleZ = 0.7f * noseZ; // offset from chassis center
+        float rearAxleZ = 0.8f * tailZ; // offset from chassis center
         float radius = 0.3f; // of each tire
         float restLength = 0.2f; // of the suspension
         float xOffset = 0.9f * halfWidth;
         Vector3f axleDirection = new Vector3f(-1f, 0f, 0f);
         Vector3f suspensionDirection = new Vector3f(0f, -1f, 0f);
-        vehicle.addWheel(new Vector3f(-xOffset, 0f, frontAxisZ),
+        vehicle.addWheel(new Vector3f(-xOffset, 0f, frontAxleZ),
                 suspensionDirection, axleDirection, restLength, radius, front);
-        vehicle.addWheel(new Vector3f(xOffset, 0f, frontAxisZ),
+        vehicle.addWheel(new Vector3f(xOffset, 0f, frontAxleZ),
                 suspensionDirection, axleDirection, restLength, radius, front);
-        vehicle.addWheel(new Vector3f(-xOffset, 0f, rearAxisZ),
+        vehicle.addWheel(new Vector3f(-xOffset, 0f, rearAxleZ),
                 suspensionDirection, axleDirection, restLength, radius, rear);
-        vehicle.addWheel(new Vector3f(xOffset, 0f, rearAxisZ),
+        vehicle.addWheel(new Vector3f(xOffset, 0f, rearAxleZ),
                 suspensionDirection, axleDirection, restLength, radius, rear);
 
         physicsSpace.addCollisionObject(vehicle);
